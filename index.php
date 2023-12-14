209b955c1d65870b061f497c4c19297179f70dcd
goldipl <mgodlewski92@gmail.com>
2023-12-14 19:34:06 +0100

    correct title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MACMA | Strona główna</title>
+        <title>MACMA - Strona główna</title>
         <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/swiper-bundle.css" />
