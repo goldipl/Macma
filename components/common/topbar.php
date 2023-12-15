c1dad1ff5f71eed23d29ddb196f0e99963e33c0b
goldipl <mgodlewski92@gmail.com>
2023-12-15 15:45:40 +0100

    add nav & topbar templates with styles

--- components/common/topbar.php
+++ components/common/topbar.php
@@ -1,5 +1,44 @@
 <div id="topbar">
-    <div class="topbar__container">
-
+    <div class="container d-flex justify-content-between align-items-center">
+        <div class="d-flex flex-row">
+            <a href="#" class="p-1">
+                <img src="./assets/icons/common/topbar/facebook.svg" alt="facebook" width="26" height="26">
+            </a>
+            <a href="#" class="p-1">
+                <img src="./assets/icons/common/topbar/youtube.svg" alt="youtube" width="26" height="26">
+            </a>
+            <a href="#" class="p-1">
+                <img src="./assets/icons/common/topbar/instagram.svg" alt="instagram" width="26" height="26">
+            </a>
+            <a href="#" class="p-1">
+                <img src="./assets/icons/common/topbar/linkedin.svg" alt="linkedin" width="26" height="26">
+            </a>
+        </div>
+        <div class="d-flex flex-row align-items-center">
+            <a href="#" class="p-3">
+                O nas
+            </a>
+            <a href="#" class="p-3">
+                Newsletter
+            </a>
+            <a href="#" class="p-3">
+                Aktualności
+            </a>
+            <a href="#" class="p-3">
+                Blog
+            </a>
+            <a href="#" class="p-3">
+                FAQ
+            </a>
+            <a href="#" class="p-3">
+                Kontakt
+            </a>
+            <a href="#" class="p-1">
+                <span class="d-flex justify-content-center align-items-center bg-white rounded-circle">PL</span>
+            </a>
+            <a href="#" class="p-1">
+                <span class="d-flex justify-content-center align-items-center bg-white rounded-circle">EN</span>
+            </a>
+        </div>
     </div>
 </div>
