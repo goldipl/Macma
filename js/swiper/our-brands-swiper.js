14fd9854c9e8ef07ee8bc165e65b7ac180ac3440
goldipl <mgodlewski92@gmail.com>
2023-12-18 15:33:21 +0100

    change space between imgs

--- js/swiper/our-brands-swiper.js
+++ js/swiper/our-brands-swiper.js
@@ -17,21 +17,25 @@ const our_brands_swiper = new Swiper('.our_brands_swiper', {
        1240: {
         slidesPerView: 5,
         slidesPerGroup: 5,
+        spaceBetween: 80,
       },
       // when window width is >= 992px
       992: {
         slidesPerView: 4,
         slidesPerGroup: 4,
+        spaceBetween: 60,
       },
       // when window width is >= 760px
       760: {
         slidesPerView: 3,
         slidesPerGroup: 3,
+        spaceBetween: 30,
       },
       // when window width is >= 200px
       200: {
         slidesPerView: 2,
         slidesPerGroup: 2,
+        spaceBetween: 20,
       },
     },
   
