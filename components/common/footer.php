76d29fcdd6f16335636b338c7a3c99133ef349aa
goldipl <mgodlewski92@gmail.com>
2023-12-19 13:06:30 +0100

    fill footer template and styling

--- components/common/footer.php
+++ components/common/footer.php
@@ -1,3 +1,67 @@
-<div class="footer__container">
-
+<div class="footer__container container">
+    <div class="footer__container__top d-flex flex-row justify-content-between">
+        <div class="d-flex flex-column col-3 first-column">
+            <div class="footer-logo">
+                <img src="./assets/icons/common/footer/logo-macma-footer.svg" alt="macma logo" width="200" height="42">
+            </div>
+            <div class="footer-text">
+                <strong>Tysiące gadżetów w jednych miejscu!</strong>
+            </div>
+            <div class="footer-text">
+                <p>Macma jest jednym z największych importerów i dostawców gadżetów reklamowych w Europie. Istniejemy już od 2003 roku, co pozwoliło nam na zdobycie doświadczenia i dużej znajomości branży reklamowej.</p>
+            </div>
+            <div class="footer-text newsletter-title">
+                <strong>Zapisz się do newslettera</strong>
+            </div>
+            <form class="newsletter-form d-inline-flex">
+                <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="newsletter" id="newsletter_input" placeholder="Wpisz adres e-mail" required>
+                <button id="newsletter_btn">Wyślij</button>
+            </form>
+        </div>
+        <div class="d-flex flex-column align-items-center col-3 second-column">
+            <div class="footer-menu-box d-flex flex-column">
+                <h2>Menu</h2>
+                <a href="#">Oferta</a>
+                <a href="#">Marki</a>
+                <a href="#">Tematyczne</a>
+                <a href="#">Promocje</a>
+                <a href="#">Znakowanie</a>
+                <a href="#">Katalogi</a>
+                <a href="#">Import</a>
+            </div>
+        </div>
+        <div class="d-flex flex-column align-items-center col-3 third-column">
+            <div class="footer-socials-box d-flex flex-column">
+                <h2>Social media</h2>
+                <a href="#"><img src="./assets/icons/common/footer/facebook.svg" alt="facebook logo" width="26" height="26">Facebook</a>
+                <a href="#"><img src="./assets/icons/common/footer/youtube.svg" alt="youtube logo" width="26" height="26">YouTube</a>
+                <a href="#"><img src="./assets/icons/common/footer/instagram.svg" alt="instagram logo" width="26" height="26">Instagram Macma</a>
+                <a href="#"><img src="./assets/icons/common/footer/instagram.svg" alt="instagram logo" width="26" height="26">Instagram M-Collection</a>
+                <a href="#"><img src="./assets/icons/common/footer/linkedin.svg" alt="linkedin logo" width="26" height="26">Linkedin</a>
+            </div>
+        </div>
+        <div class="d-flex flex-column align-items-center col-3 fourth-column">
+            <div class="footer-contacts-box d-flex flex-column">
+                <h2>Kontakt</h2>
+                <div class="footer-contact-box">
+                    <strong>Biuro</strong>
+                    <p>tel. <a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                    <p>e-mail: <a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+                </div>
+                <div class="footer-contact-box">
+                    <strong>Drukarnia</strong>
+                    <p>tel. <a href="tel:+48 71 397 87 00">+48 71 397 87 00</a></p>
+                    <p>e-mail: <a href="mailto:drukarnia@macma.pl">drukarnia@macma.pl</a></p>
+                </div>
+                <div class="footer-contact-box">
+                    <strong>Magazyn</strong>
+                    <p>tel. <a href="tel:+48 71 397 87 18">+48 71 397 87 18</a></p>
+                    <p>e-mail: <a href="mailto:magazyn2@macma.pl">magazyn2@macma.pl</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__container__bottom d-flex justify-content-center align-items-center">
+        <p>© 2009 - <span></span> Macma Sp. z o. o.</p>
+    </div>
 </div>
