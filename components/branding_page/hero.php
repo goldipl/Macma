37ec3b79d0e4da92634750c2a3f47b9fd245332a
goldipl <mgodlewski92@gmail.com>
2023-12-22 13:22:44 +0100

    styling & fill hero template

--- components/branding_page/hero.php
+++ components/branding_page/hero.php
@@ -1,9 +1,9 @@
-<section id="hero" class="hero">
+<section id="hero" class="hero container">
     <div class="hero_content">
-        <h1 class="hero_content--title"></h1>
-        <p class="hero_content--text"></p>
+        <h1 class="hero_content--title">Znakowanie</h1>
+        <p class="hero_content--text">Aby upominek w pełni spełniał funkcje reklamowe koniecznie powinien zostać spersonalizowany. Idealnym rozwiązaniem jest trwały logotyp wykonany na gadżecie, który gwarantuje skuteczną reklamę na lata! Dbając o wygodę naszych Klientów świadczymy kompleksowe usługi w zakresie znakowania upominków reklamowych. Zajmujemy się znakowaniem artykułów własnych oraz powierzonych.</p>
         <div class="hero_content--btn">
-            <a href="#">Kliknij i sprawdź</a>
+            <a href="#">Kliknij i sprawdź<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
         </div>
     </div>
     <div class="hero_image">
