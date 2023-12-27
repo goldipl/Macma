61494a1d0d4481dd56cdfec18db9af7eed5f81ac
goldipl <mgodlewski92@gmail.com>
2023-12-27 15:50:55 +0100

    styling and add branding dates subpage

---
+++ components/branding_page/branding_table.php
@@ -0,0 +1,51 @@
+<div class="branding-table container">
+    <h1>Terminy znakowań</h1>
+    <table>
+        <tbody>
+            <tr>
+                <th>Znakowanie</th>
+                <th>Terminy znakowania dla towarów zamówionych dzisiaj do godz. 12:00</th>
+            </tr>
+            <tr>
+                <td>Doming</td>
+                <td>poniedziałek, 2 października 2023</td>
+            </tr>
+            <tr>
+                <td>Druk DTG</td>
+                <td>piątek, 29 września 2023</td>
+            </tr>
+            <tr>
+                <td>Grawer laserowy</td>
+                <td>czwartek, 28 września 2023</td>
+            </tr>
+            <tr>
+                <td>Haft komputerowy</td>
+                <td>poniedziałek, 2 października 2023</td>
+            </tr>
+            <tr>
+                <td>Kalka ceramiczna</td>
+                <td>poniedziałek, 2 października 2023</td>
+            </tr>
+            <tr>
+                <td>Sitodruk</td>
+                <td>piątek, 29 września 2023</td>
+            </tr>
+            <tr>
+                <td>Sitodruk na tekstyliach</td>
+                <td>piątek, 29 września 2023</td>
+            </tr>
+            <tr>
+                <td>Sublimacja</td>
+                <td>piątek, 29 września 2023</td>
+            </tr>
+            <tr>
+                <td>Tampodruk</td>
+                <td>piątek, 29 września 2023</td>
+            </tr>
+            <tr>
+                <td>Termotransfer</td>
+                <td>piątek, 29 września 2023</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
