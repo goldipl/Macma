b07ccb533ee27d7bdbe06dac287a001129eaa26a
goldipl <mgodlewski92@gmail.com>
2023-12-27 15:26:17 +0100

    add article gallery script

---
+++ js/article_gallery/article_gallery.js
@@ -0,0 +1,25 @@
+let gallery_top = new Swiper(".article-gallery", {
+  slidesPerView: 1,
+  freeMode: false,
+  loop: false,
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+  },
+  // Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
+
+let gallery_thumbs = new Swiper(".gallery-thumbs", {
+  slidesPerView: "auto",
+  // centeredSlides: true,
+  spaceBetween: 20,
+  loop: false,
+  slideToClickedSlide: true,
+});
+
+gallery_top.controller.control = gallery_thumbs;
+gallery_thumbs.controller.control = gallery_top;
