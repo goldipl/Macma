0d459f611d62be1282900483f1f2565efb6e8d6a
goldipl <mgodlewski92@gmail.com>
2023-12-27 12:00:40 +0100

    fill branding articles templates and add styles

--- components/branding_page/branding_articles.php
+++ components/branding_page/branding_articles.php
@@ -1,3 +1,212 @@
-<section id="branding_articles">
-    
+<section class="branding_articles container d-flex flex-wrap flex-row">
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article01.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Druk DTF</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>DTG, inaczej Direct to Garment to nowoczesna technologia nadruków bezpośrednich, która sprawdza się przede wszystkim na tekstyliach takich, jak koszulki i torby.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article02.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Druk DTG</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>DTG, inaczej Direct to Garment to nowoczesna technologia nadruków bezpośrednich, która sprawdza się przede wszystkim na tekstyliach takich, jak koszulki i torby.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article03.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Druk UV</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>DTG, inaczej Direct to Garment to nowoczesna technologia nadruków bezpośrednich, która sprawdza się przede wszystkim na tekstyliach takich, jak koszulki i torby.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article04.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Grawerowanie laserowe</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>DTG, inaczej Direct to Garment to nowoczesna technologia nadruków bezpośrednich, która sprawdza się przede wszystkim na tekstyliach takich, jak koszulki i torby.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article05.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Haft komputerowy</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Haft to jeden z najstarszych sposobów zdobienia przedmiotów. Następca ręcznego haftu - haft komputerowy jest dziś powszechnie stosowany do znakowania upominków reklamowych.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article06.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Kalka ceramiczna</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Znakowanie za pomocą kalki ceramicznej polega na nałożeniu specjalnego papieru, na którym znajdują się obrazy (np. logotypy, napisy) na powierzchnię ceramicznego przedmiotu (np. kubka).</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article07.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Tampodruk</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Metoda tampodruku polega na nakładaniu trwałej farby drukarskiej za pomocą miękkiego, gładkiego stempla zwanego tamponem na znakowany artykuł reklamowy.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article08.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Sitodruk</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Technika sitodruku ma bardzo długą historię - wywodzi się z tzw. malowania szablonowego, znanego już w czasach starożytnych. W metodzie tej kluczowym urządzeniem jest sito.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article09.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Sitodruk na tekstyliach</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Automatyczny druk wielokolorowy wykonywany na tekstyliach przy pomocy profesjonalnej karuzeli. Nadruki nanoszone są na wszelkiego rodzaju odzieży reklamowej (t-shirty, koszulki polo, bluzy) i torbach bawełnianych.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article10.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Termotransfer</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Termotransfer to technika znakowania polegająca na termicznym wgrzaniu logotypu w dany materiał. Początkowo na specjalnym papierze transferowym przygotowywany jest logotyp,</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article11.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Tłoczenie</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Technika znakowania polegająca na przeniesieniu wybranego wzoru z matrycy na produkt przy udziale dużego ciśnienia i wysokiej temperatury. Tłoczenie wykonywane jest zazwyczaj na wszelkiego rodzaju materiałach skórzanych</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article12.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Sublimacja</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Sublimacja na ceramice to metoda znakowania polegająca na przeniesieniu tuszu z wydruku bezpośrednio na powierzchnię specjalnie przygotowanego kubka.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article13.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Doming</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Doming (naklejka żywiczna) to technologia znakowania pozwalająca na uzyskanie efektu niemal trójwymiarowego obrazu. Polega na nałożeniu na kolorowy wydruk cyfrowy warstwy żywicy poliuretanowej..</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article14.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Etykiety i zawieszki</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Jedną z możliwości wykorzystania technologii znakowania laserowego i druku cyfrowego jest wykonywanie różnego rodzaju etykiet i zawieszek. Podczas znakowania gadżetów za pomocą etykiet możemy posłużyć się plikami.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="branding_articles__slot d-flex flex-column align-items-center flex-col">
+        <div class="branding_articles__slot--img">
+            <img src="./assets/img/branding_page/article15.jpg" alt="article img" width="499" height="242">
+        </div>
+        <div class="branding_articles__slot--title">
+            <h2>Towar powierzony</h2>
+        </div>
+        <div class="branding_articles__slot--text">
+            <p>Technika sitodruku ma bardzo długą historię - wywodzi się z tzw. malowania szablonowego, znanego już w czasach starożytnych. W metodzie tej kluczowym urządzeniem jest sito.</p>
+        </div>
+        <div class="branding_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
 </section>
