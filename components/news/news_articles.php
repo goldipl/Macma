5d9caa6e144033af5b36268fd9dcceaf288fc7f3
goldipl <mgodlewski92@gmail.com>
2023-12-28 15:57:59 +0100

    styling news articles slots and all slots to news_article template

--- components/news/news_articles.php
+++ components/news/news_articles.php
@@ -33,4 +33,174 @@
             <a href="#">Zobacz więcej</a>
         </div>
     </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_03.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Ekipa do zadań specjalnych</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Poznajcie pierwszoplanowe gwiazdy, które wcielą się w każdą rolę, żeby zareklamować biznes Waszych Klientów!</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_04.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>PROMO SHOW 8-9 lutego 2023 - czekamy na Was!</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Zapraszamy wszystkich na nasze stoisko, a tych którzy nie mogą być z nami prosimy o cierpliwość.</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_01.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Ulepszamy naszą drukarnię!</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Nadruki i gadżety reklamowe to niesamowicie skuteczne połączenie. Żeby zapewnić Wam jak najwyższą jakość i jak najlepszy serwis podjęliśmy decyzję o rozbudowie naszej drukarni</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_02.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Złota noc</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Jako hurtownia gadżetów reklamowych działamy nieprzerwanie już od 20 lat! Niedawno mieliśmy okazją śwętować ten okrągły jubileusz.</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_03.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Ekipa do zadań specjalnych</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Poznajcie pierwszoplanowe gwiazdy, które wcielą się w każdą rolę, żeby zareklamować biznes Waszych Klientów!</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_04.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>PROMO SHOW 8-9 lutego 2023 - czekamy na Was!</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Zapraszamy wszystkich na nasze stoisko, a tych którzy nie mogą być z nami prosimy o cierpliwość.</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_01.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Ulepszamy naszą drukarnię!</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Nadruki i gadżety reklamowe to niesamowicie skuteczne połączenie. Żeby zapewnić Wam jak najwyższą jakość i jak najlepszy serwis podjęliśmy decyzję o rozbudowie naszej drukarni</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_02.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Złota noc</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Jako hurtownia gadżetów reklamowych działamy nieprzerwanie już od 20 lat! Niedawno mieliśmy okazją śwętować ten okrągły jubileusz.</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_03.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>Ekipa do zadań specjalnych</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Poznajcie pierwszoplanowe gwiazdy, które wcielą się w każdą rolę, żeby zareklamować biznes Waszych Klientów!</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
+    <div class="news_articles__slot d-flex flex-column align-items-start flex-col">
+        <div class="news_articles__slot--img">
+            <img src="./assets/img/news/news_article_04.jpg" alt="article img" width="372" height="206">
+        </div>
+        <div class="news_articles__slot--date">
+            <p>Poniedziałek, 12 czerwca 2023</p>
+        </div>
+        <div class="news_articles__slot--title">
+            <h2>PROMO SHOW 8-9 lutego 2023 - czekamy na Was!</h2>
+        </div>
+        <div class="news_articles__slot--text">
+            <p>Zapraszamy wszystkich na nasze stoisko, a tych którzy nie mogą być z nami prosimy o cierpliwość.</p>
+        </div>
+        <div class="news_articles__slot--link">
+            <a href="#">Zobacz więcej</a>
+        </div>
+    </div>
 </div>
