06ca4c546fc3740644a1e4e8393fda3420cd3f36
goldipl <mgodlewski92@gmail.com>
2023-12-28 14:16:32 +0100

    add and styling newsletter hero subpage

---
+++ components/newsletter/hero.php
@@ -0,0 +1,24 @@
+<section id="hero" class="hero container newsletter-pink">
+    <div class="hero_content">
+        <div class="breadcrumbs container">
+            <ul>
+                <li class="breadcrumb-item">
+                    <a href="./index.php">Home</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="#">Newsletter</a>
+                </li>
+            </ul>
+        </div>
+        <h1 class="hero_content--title">Newsletter</h1>
+        <p class="hero_content--text">Zapisz się do naszego newsletter i bądź na bieżąco z aktualnościami</p>
+        <form>
+            <input type="email" name="newsletter-hero" id="newsletter-hero" class="newsletter-hero" required>
+            <button class="orange-btn">Wyślij <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+        </form>
+    </button>
+    </div>
+    <div class="hero_image">
+        <img src="./assets/img/newsletter/newsletter_hero.jpg" alt="hero img" width="1928" height="638">
+    </div>
+</section>
