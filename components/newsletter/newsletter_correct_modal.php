d51f25c9d40e052b077c26248070d20401f506b0
goldipl <mgodlewski92@gmail.com>
2023-12-28 14:38:08 +0100

    add incorrect/correct mail templates & scss styles

---
+++ components/newsletter/newsletter_correct_modal.php
@@ -0,0 +1,4 @@
+<div class="newsletter_correct_modal container">
+    <img src="./assets/icons/newsletter/correct-email-icon.svg" alt="correct icon" width="58" height="58">
+    <strong>Dziękujemy za zapisanie się do newslettera</strong>
+</div>
---
+++ components/newsletter/newsletter_incorrect_modal.php
@@ -0,0 +1,4 @@
+<div class="newsletter_incorrect_modal container">
+    <img src="./assets/icons/newsletter/incorrect-email-icon.svg" alt="incorrect icon" width="58" height="58">
+    <strong>Wpisz poprawny adres email</strong>
+</div>
