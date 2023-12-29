81572327b5bd6f22b1bf684137a4f4cd9225a247
goldipl <mgodlewski92@gmail.com>
2023-12-29 18:17:50 +0100

    fill and styling product card info

--- components/product_card/product_card_gallery_info.php
+++ components/product_card/product_card_gallery_info.php
@@ -48,6 +48,79 @@
         </div>
     </div>
     <div class="product-info">
-
+        <div class="product-info_title">Długopis metalowy</div>
+        <div class="product-info_code">Kod: <span>1333903</span></div>
+        <div class="product-info_color">Kolor: <span>czarny</span></div>
+        <div class="product-info_ean">EAN: <span>4064571004023</span></div>
+        <p class="product-info_availability-colors-title">Dostępne kolory:</p>
+        <div class="product-info_availability-colors">
+            <a href="#" class="color grey"></a>
+            <a href="#" class="color red"></a>
+            <a href="#" class="color yellow"></a>
+            <a href="#" class="color lightblue"></a>
+            <a href="#" class="color blue"></a>
+            <a href="#" class="color pink"></a>
+            <a href="#" class="color purple"></a>
+            <a href="#" class="color green"></a>
+            <a href="#" class="color black checked"></a>
+        </div>
+        <div class="product-price-box">
+            <div class="product-price-box_top">
+                <div class="product-price-box_top__left">
+                    <div class="catalog-price">
+                        <span>Cena katalogowa <p>2,88 PLN</p></span>
+                    </div>
+                    <div class="your-price">
+                        <span>Twoja cena <p>2,55 PLN</p></span>
+                    </div>
+                </div>
+                <a href="" class="product-price-box_top__right">
+                    <img src="./assets/icons/product_page/calculator.svg" alt="calc icon">
+                    <p>Kalkulator</p>
+                </a>
+            </div>
+            <div class="product-price-box_bottom">
+                <div class="product-price-box_bottom__left">
+                    <span>Cena promocyjna <p>1,88 PLN</p></span>
+                    <p class="minimal-quantity">Minimalna ilość: 500 szt.</p>
+                </div>
+                <button class="product-price-box_bottom__right">
+                    <img src="./assets/icons/product_page/cart.svg" alt="cart icon">
+                    <p>Dodaj do koszyka</p>
+                </button>
+            </div>
+        </div>
+        <div class="product-more-box">
+            <div class="product-more-box__row">
+                <a href="#" class="product-more-box__col">
+                    <img src="./assets/icons/product_page/nego-price.svg" alt="download icon">
+                    <p>Negocjuj cenę</p>
+                </a>
+                <a href="#" class="product-more-box__col">
+                    <img src="./assets/icons/product_page/comparison.svg" alt="comparision icon">
+                    <p>Porównywarka</p>
+                </a>
+            </div>
+            <div class="product-more-box__row">
+                <a href="#" class="product-more-box__col">
+                    <img src="./assets/icons/product_page/offert.svg" alt="download icon">
+                    <p>Ofertownik</p>
+                </a>
+                <a href="#" class="product-more-box__col">
+                    <img src="./assets/icons/product_page/download.svg" alt="comparision icon">
+                    <p>Pobierz ulotkę PDF</p>
+                </a>
+            </div>
+            <div class="product-more-box__row last-row">
+                <a href="#" class="product-more-box__col">
+                    <img src="./assets/icons/product_page/branding-time.svg" alt="download icon">
+                    <p>Znakowanie: <span>2 dni</span></p>
+                </a>
+                <a href="#" class="product-more-box__col">
+                    <img src="./assets/icons/product_page/branding-location.svg" alt="comparision icon">
+                    <p>Miejsce znakowania</p>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
