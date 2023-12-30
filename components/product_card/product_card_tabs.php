60ebbc61a9c3496fdc0c27b903562eb0926a9605
goldipl <mgodlewski92@gmail.com>
2023-12-30 11:12:52 +0100

    fill & styling product table

--- components/product_card/product_card_tabs.php
+++ components/product_card/product_card_tabs.php
@@ -1,3 +1,136 @@
-<div class="product_card_tabs">
-    
+<div class="product_card_tabs container">
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <button class="nav-link active" id="nav-desc-tab" data-bs-toggle="tab" data-bs-target="#nav-desc" type="button" role="tab" aria-controls="nav-desc" aria-selected="true">Opis i stan magazynowy</button>
+            <button class="nav-link" id="nav-branding-tab" data-bs-toggle="tab" data-bs-target="#nav-branding" type="button" role="tab" aria-controls="nav-branding" aria-selected="false">Znakowanie</button>
+            <button class="nav-link" id="nav-specification-tab" data-bs-toggle="tab" data-bs-target="#nav-specification" type="button" role="tab" aria-controls="nav-specification" aria-selected="false">Specyfikacja</button>
+            <button class="nav-link" id="nav-packing-tab" data-bs-toggle="tab" data-bs-target="#nav-packing" type="button" role="tab" aria-controls="nav-packing" aria-selected="false">Pakowanie</button>
+            <button class="nav-link" id="nav-detdesc-tab" data-bs-toggle="tab" data-bs-target="#nav-detdesc" type="button" role="tab" aria-controls="nav-detdesc" aria-selected="false">Opis szczegółowy</button>
+            <button class="nav-link" id="nav-detdesc-tab" data-bs-toggle="tab" data-bs-target="#nav-todownload" type="button" role="tab" aria-controls="nav-todownload" aria-selected="false">Do pobrania</button>
+            <button class="nav-link" id="nav-detdesc-tab" data-bs-toggle="tab" data-bs-target="#nav-movies" type="button" role="tab" aria-controls="nav-movies" aria-selected="false">Filmy</button>
+        </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-desc" role="tabpanel" aria-labelledby="nav-desc-tab" tabindex="0">
+            <p>Klasyczny długopis metalowy z chromowanymi elementami i smukłym klipsem. Długopis wyposażony w duży niebieski wkład.</p>
+            <h2>Dostępność</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Kolor</th>
+                        <th>24 godziny</th>
+                        <th>2-3 dni</th>
+                        <th>Dostawa</th>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color pink"></div>363902 (różowy)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+5 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color red"></div>363902 (czerwony)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+25 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color green"></div>363902 (zielony)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+15 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="active">
+                        <td><a href="#"><div class="color black"></div>363902 (czarny)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+45 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color blue"></div>363902 (niebieski)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+45 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color yellow"></div>363902 (żółty)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+45 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color purple"></div>363902 (fioletowy)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <a class="mail-contact" href="#">
+                                    <img src="./assets/icons/product_page/orange-envelope.svg" alt="envelope">Kontakt
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#"><div class="color lightblue"></div>363902 (jasnoniebieski)</a></td>
+                        <td>22 067 szt.</td>
+                        <td>11 739 szt.</td>
+                        <td>
+                            <div class="two-lines">
+                                <p>+45 000</p>
+                                <span>23.01-29.01.2024</span>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="tab-pane fade" id="nav-branding" role="tabpanel" aria-labelledby="nav-branding-tab" tabindex="0">
+
+        </div>
+        <div class="tab-pane fade" id="nav-specification" role="tabpanel" aria-labelledby="nav-specification-tab" tabindex="0">
+
+        </div>
+        <div class="tab-pane fade" id="nav-packing" role="tabpanel" aria-labelledby="nav-packing-tab" tabindex="0">
+
+        </div>
+        <div class="tab-pane fade" id="nav-detdesc" role="tabpanel" aria-labelledby="nav-detdesc-tab" tabindex="0">
+
+        </div>
+        <div class="tab-pane fade" id="nav-todownload" role="tabpanel" aria-labelledby="nav-todownload-tab" tabindex="0">
+
+        </div>
+        <div class="tab-pane fade" id="nav-movies" role="tabpanel" aria-labelledby="nav-movies-tab" tabindex="0">
+
+        </div>
+    </div>
 </div>
