4effcbeb80bf4cb26da60f764fe4b5052de29ef6
goldipl <mgodlewski92@gmail.com>
2023-12-31 15:09:36 +0100

    add cart step two starter templates

---
+++ components/cart/cart_step_two_content.php
@@ -0,0 +1,37 @@
+<div class="cart_container step_one">
+    <div class="container">
+        <h1>Znakowanie</h1>
+        <div class="cart-table_full_box d-flex flex-row">
+            <div class="cart-table_box d-flex flex-column">
+                <div class="cart-step-table-container">
+                    <table class="cart-products-table">
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+                <div class="bottom-cart-box-with-btn">
+                    <div class="bottom-cart-button">
+                        <a href="#" class="white-btn next left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
+                    </div>
+                    <div class="bottom-cart-button">
+                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                    </div>
+                </div>
+            </div>
+            <aside class="sumary_box">
+                <div class="sumary_box--title">
+                    <p>CAŁKOWITA WARTOŚĆ PRODUKTÓW ZE ZNAKOWANIEM</p>
+                </div>
+                <div class="sumary_box--netto">
+                    <p class="netto-value">17,88 PLN</p>
+                    <p class="netto-text">Suma netto</p>
+                </div>
+                <div class="sumary_box--brutto">
+                    <p class="brutto-value">35,66 PLN</p>
+                    <p class="brutto-text">Suma brutto</p>
+                </div>
+            </aside>
+        </div>
+    </div>
+</div>
