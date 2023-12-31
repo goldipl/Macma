235467fe015564fe1382243fee82393df91f4d9e
goldipl <mgodlewski92@gmail.com>
2023-12-31 14:51:53 +0100

    change first col min-width to 40px

--- components/cart/cart_step_one_content.php
+++ components/cart/cart_step_one_content.php
@@ -18,7 +18,7 @@
                                 <th>Usuń</th>
                             </tr>
                             <tr>
-                                <td>1</td>
+                                <td class="table-number-col">1</td>
                                 <td class="table-product-col">
                                     <div class="table-product-box">
                                         <div class="table-product-box__top">
@@ -51,7 +51,7 @@
                                 </td>
                             </tr>
                             <tr>
-                                <td>2</td>
+                                <td class="table-number-col">2</td>
                                 <td class="table-product-col">
                                     <div class="table-product-box">
                                         <div class="table-product-box__top">
