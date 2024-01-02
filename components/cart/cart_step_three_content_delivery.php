59a39874e05087412e4d9420623e82e8c54e9865
goldipl <mgodlewski92@gmail.com>
2024-01-02 16:03:25 +0100

    add step three cart content and pre styles

---
+++ components/cart/cart_step_three_content_delivery.php
@@ -0,0 +1,49 @@
+<div class="cart_container step_three">
+    <div class="container">
+        <h1>Wysyłka</h1>
+        <div class="cart-table_full_box d-flex flex-row">
+            <div class="cart-table_box d-flex flex-column">
+                <div class="cart-step-table-container">
+                    <table class="cart-products-table">
+                        <tbody>
+                            <!-- === Product 01 === -->
+                            <tr>
+                                <td><b>1</b></td>
+                                <td>
+                                    <div class="cart-delivery-product-box">
+                                        <div class="cart-delivery-product-box__top">
+                                            <div class="cart-delivery-product-box__top__left">
+                                                <a href="#" class="title">Długopis metalowy</a>
+                                                <p class="code">Kod: <span>1333903</span></p>
+                                            </div>
+                                        </div>
+                                        <div class="cart-delivery-product-box__bottom">
+                                            <div class="cart-delivery__col">
+                                                <a href="#" class="cart-branding-product-box__middle__img">
+                                                    <img src="./assets/img/cart/pen.jpg" alt="product img" width="110" height="110">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <!-- === Product 02 === -->
+                            <tr>
+                                <td><b>2</b></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="bottom-cart-box-with-btn">
+                    <div class="bottom-cart-button">
+                        <a href="#" class="white-btn next left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
+                    </div>
+                    <div class="bottom-cart-button right-btn">
+                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ components/cart/cart_step_three_content_free_delivery.php
@@ -0,0 +1,33 @@
+<div class="cart_container step_three">
+    <div class="container">
+        <h1>Wysyłka</h1>
+        <div class="cart-table_full_box d-flex flex-row">
+            <div class="cart-table_box d-flex flex-column">
+                <div class="cart-step-table-container">
+                    <table class="cart-products-table">
+                        <tbody>
+                            <!-- === Product 01 === -->
+                            <tr>
+                                <td><b>1</b></td>
+                                <td></td>
+                            </tr>
+                            <!-- === Product 02 === -->
+                            <tr>
+                                <td><b>2</b></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="bottom-cart-box-with-btn">
+                    <div class="bottom-cart-button">
+                        <a href="#" class="white-btn next left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
+                    </div>
+                    <div class="bottom-cart-button right-btn">
+                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
