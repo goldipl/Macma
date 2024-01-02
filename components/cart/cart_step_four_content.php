bae4122da0a615ef5551d9019c63425fb3c0ebc7
goldipl <mgodlewski92@gmail.com>
2024-01-02 19:42:09 +0100

    add cart step four starer files

---
+++ components/cart/cart_step_four_content.php
@@ -0,0 +1,20 @@
+<div class="cart_container step_four">
+    <div class="container">
+        <h1>Twoje Dane</h1>
+        <div class="cart-table_full_box d-flex flex-row">
+            <div class="cart-table_box d-flex flex-column">
+                <div class="cart-your-data-container">
+
+                </div>
+                <div class="bottom-cart-box-with-btn">
+                    <div class="bottom-cart-button">
+                        <a href="#" class="white-btn prev left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
+                    </div>
+                    <div class="bottom-cart-button right-btn">
+                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
