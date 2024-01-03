ab8ae24cb1bdc8370fb883d814c9bd1993b0fc56
goldipl <mgodlewski92@gmail.com>
2024-01-03 16:56:22 +0100

    fill main template and add main styles

--- components/calculator/calculator_content.php
+++ components/calculator/calculator_content.php
@@ -2,19 +2,19 @@
     <div class="container">
         <h1>Kalkulator</h1>
         <div class="d-flex flex-row">
-            <div class="d-flex flex-column">
+            <div class="accordion-container d-flex flex-column">
                 <div class="accordion me-2" id="accordionExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading11">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse11" aria-expanded="true" aria-controls="collapse11"> 1.1 Kto może podjąć z nami współpracę? </button>
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse11" aria-expanded="true" aria-controls="collapse11"><span class="number">1</span>Długopis metalowy<p class="code">Kod:<span>1333903</span></p></button>
                         </h2>
-                        <div id="collapse11" class="accordion-collapse collapse show" aria-labelledby="heading11" data-bs-parent="#accordionExample">
+                        <div id="collapse11" class="accordion-collapse collapse" aria-labelledby="heading11" data-bs-parent="#accordionExample">
                             <div class="accordion-body"> Nasza oferta jest skierowana wyłącznie do agencji reklamowych oraz firm pośredniczących w branży reklamowej, prowadzących działania promocyjne, kampanie reklamowe, organizujących eventy, a także zajmujących się projektowaniem i sprzedażą gadżetów reklamowych. </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading12">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse12" aria-expanded="false" aria-controls="collapse12"> 1.2 Jak rozpocząć z nami współpracę? </button>
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse12" aria-expanded="false" aria-controls="collapse12"><span class="number">2</span>Wybierz kolory<img src="./assets/icons/common/check-circle-fill.svg" alt="check-circle icon" width="24" height="24"></button>
                         </h2>
                         <div id="collapse12" class="accordion-collapse collapse" aria-labelledby="heading12" data-bs-parent="#accordionExample">
                             <div class="accordion-body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
@@ -22,7 +22,7 @@
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading13">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse13" aria-expanded="false" aria-controls="collapse13"> 1.3 W jaki sposób można się zarejestrować na stronie www.macma.pl? </button>
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse13" aria-expanded="false" aria-controls="collapse13"><span class="number">3</span>Podaj ilość<img src="./assets/icons/common/check-circle-fill.svg" alt="check-circle icon" width="24" height="24"><p class="quantity">Ilość całkowita:<span>1000 szt.</span></p></button>
                         </h2>
                         <div id="collapse13" class="accordion-collapse collapse" aria-labelledby="heading13" data-bs-parent="#accordionExample">
                             <div class="accordion-body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
@@ -30,7 +30,7 @@
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading14">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse14" aria-expanded="false" aria-controls="collapse14"> 1.4 Jakie są korzyści z rejestracji w STREFIE DLA AGENCJI?</button>
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse14" aria-expanded="false" aria-controls="collapse14"><span class="number">4</span>Kliknij i wybierz technikę oraz miejsce znakowania</button>
                         </h2>
                         <div id="collapse14" class="accordion-collapse collapse" aria-labelledby="heading14" data-bs-parent="#accordionExample">
                             <div class="accordion-body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
@@ -38,7 +38,7 @@
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading15">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse15" aria-expanded="false" aria-controls="collapse15"> 1.5 Dlaczego wymagamy przesłania dokumentów firmy?</button>
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse15" aria-expanded="false" aria-controls="collapse15"><span class="number">5</span>Wycena</button>
                         </h2>
                         <div id="collapse15" class="accordion-collapse collapse" aria-labelledby="heading15" data-bs-parent="#accordionExample">
                             <div class="accordion-body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
@@ -46,7 +46,7 @@
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading16">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse16" aria-expanded="false" aria-controls="collapse16"> 1.6 Dlkhkjhkhjkhjkjh jjh?</button>
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse16" aria-expanded="false" aria-controls="collapse16"><span class="number">6</span>Wycena szczegółowa</button>
                         </h2>
                         <div id="collapse16" class="accordion-collapse collapse" aria-labelledby="heading16" data-bs-parent="#accordionExample">
                             <div class="accordion-body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
@@ -59,12 +59,14 @@
                     <p>Kalkulacja</p>
                 </div>
                 <div class="sumary_box--netto">
-                    <p class="netto-value">17,88 PLN</p>
-                    <p class="netto-text">Suma netto</p>
+                    <p class="netto-value">2180,00 PLN</p>
+                    <p class="netto-value small">(2,18 PLN/szt.)</p>
+                    <p class="netto-text">Całkowita wartość produktów ze<br> znakowaniem netto</p>
                 </div>
                 <div class="sumary_box--brutto">
-                    <p class="brutto-value">21,99 PLN</p>
-                    <p class="brutto-text">Suma brutto</p>
+                    <p class="brutto-value">1090,99 PLN</p>
+                    <p class="brutto-value small">(2,18 PLN/szt.)</p>
+                    <p class="brutto-text">W tym koszt znakowania netto</p>
                 </div>
             </aside>
         </div>
