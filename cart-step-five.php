5f38f441e8aa6cdbc60b33d2fc92d96853de3379
goldipl <mgodlewski92@gmail.com>
2024-01-03 10:07:30 +0100

    add cart step five starter php & scss files

---
+++ cart-step-five.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MACMA - Koszyk - Finalizacja</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/select2.min.css" />
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body>
+        <header> 
+            <?php include "./components/common/topbar.php"; ?> 
+            <?php include "./components/common/nav.php"; ?> 
+        </header>
+        <main id="main-wrapper">
+            <div class="progress cart-progress-bar">
+                <div class="progress-bar" role="progressbar" aria-label="Basic example" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="cart-steps-container">
+                <div class="container">
+                    <div class="cart-steps-container-slot">
+                        <p>1. Produkty</p>
+                    </div>
+                    <div class="cart-steps-container-slot">
+                        <p>2. Znakowanie</p>
+                    </div>
+                    <div class="cart-steps-container-slot">
+                        <p>3. Wysyłka</p>
+                    </div>
+                    <div class="cart-steps-container-slot">
+                        <p>4. Twoje dane</p>
+                    </div>
+                    <div class="cart-steps-container-slot">
+                        <p class="active">5. Finalizacja</p>
+                    </div>
+                </div>
+            </div>
+            <?php include "./components/cart/cart_step_five_content.php"; ?> 
+        </main>
+        <footer> 
+            <?php include "./components/common/footer.php"; ?> 
+        </footer>
+        <script src="./js/jquery.min.js"></script>
+        <script src="./js/popper.min.js"></script>
+        <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="./js/bootbox.min.js"></script>
+        <script src="./js/select2.min.js"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
---
+++ components/cart/cart_step_five_content.php
@@ -0,0 +1,24 @@
+<div class="cart_container step_five">
+    <div class="container">
+        <h1>Finalizacja</h1>
+        <div class="cart-table_full_box d-flex flex-row">
+            <div class="cart-table_box d-flex flex-column">
+                <div class="cart-your-data-container">
+                    <form class="cart-your-data-container__col">
+                        <div class="title">
+                            <p>Wartość zamówienia</p>
+                        </div>
+                        <div class="col-container">
+
+                        </div>
+                    </form>
+                </div>
+                <div class="bottom-cart-box-with-btn">
+                    <div class="bottom-cart-button">
+                        <a href="#" class="white-btn prev left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
