f93a47b3b949e8598af72e3f51add8ee4a816a2b
goldipl <mgodlewski92@gmail.com>
2024-01-03 12:23:52 +0100

    add calculator page starter php & scss files

---
+++ components/calculator/calculator_content.php
@@ -0,0 +1,23 @@
+<div class="cart_container step_one">
+    <div class="container">
+        <h1>Kalkulator</h1>
+        <div class="d-flex flex-row">
+            <div class="d-flex flex-column">
+                
+            </div>
+            <aside class="sumary_box">
+                <div class="sumary_box--title">
+                    <p>Kalkulacja</p>
+                </div>
+                <div class="sumary_box--netto">
+                    <p class="netto-value">17,88 PLN</p>
+                    <p class="netto-text">Suma netto</p>
+                </div>
+                <div class="sumary_box--brutto">
+                    <p class="brutto-value">21,99 PLN</p>
+                    <p class="brutto-text">Suma brutto</p>
+                </div>
+            </aside>
+        </div>
+    </div>
+</div>
