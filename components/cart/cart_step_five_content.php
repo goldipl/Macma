3d5deff6467d437090496e42c7ce3aa4ba4ca12a
goldipl <mgodlewski92@gmail.com>
2024-01-03 12:11:27 +0100

    fill & styling cart step five

--- components/cart/cart_step_five_content.php
+++ components/cart/cart_step_five_content.php
@@ -3,16 +3,90 @@
         <h1>Finalizacja</h1>
         <div class="cart-table_full_box d-flex flex-row">
             <div class="cart-table_box d-flex flex-column">
-                <div class="cart-your-data-container">
-                    <form class="cart-your-data-container__col">
+                <form class="cart-final-container">
+                    <div class="cart-final-container__col">
                         <div class="title">
                             <p>Wartość zamówienia</p>
                         </div>
                         <div class="col-container">
-
+                            <div class="col-container_prices">
+                                <p>Wartość netto: <span>17,88 PLN</span></p>
+                                <p>Wartość brutto: <span>17,88 PLN</span></p>
+                                <p>Koszt wysyłki netto: <span>17,88 PLN</span></p>
+                                <p>Gwarantowana usługa kurierska 24h: <span>+5,00 PLN</span></p>
+                            </div>
+                            <div class="col-container_line"></div>
+                            <div class="col-container_summary-price">
+                                <p>Razem do zapłaty:</p>
+                                <p><span>17,88 PLN</span> brutto</p>
+                            </div>
+                            <div class="col-container_discount-code">
+                                <label for="discount">Wpisz swój kod rabatowy</label>
+                                <div class="input-button">
+                                    <input class="form-input" type="text" name="discount" id="discount" placeholder="WPISZ KOD">
+                                    <button for="discount">Zapisz</button>
+                                </div>
+                            </div>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="cart-final-container__col">
+                        <div class="title">
+                            <p>Dostawa</p>
+                        </div>
+                        <div class="col-container middle">
+                            <div class="final-radios-types">
+                                <label class="custom-radio-container one-logo">Dostawa kurierem
+                                    <input type="radio" name="radio" class="custom_input">
+                                    <span class="custom-radio-checkmark"></span>
+                                </label>
+                                <label class="custom-radio-container two-logos">Odbiór osobisty
+                                    <input type="radio" name="radio" class="custom_input" checked>
+                                    <span class="custom-radio-checkmark"></span>
+                                </label>
+                            </div>
+                            <div class="email-box">
+                                <h3>Email</h3>
+                                <p class="desc">na który zostanie wysłane potwierdzenia zamówienia</p>
+                                <input type="email" class="form-input" name="email" id="email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cart-final-container__col">
+                        <div class="title">
+                            <p>Forma płatności</p>
+                        </div>
+                        <div class="col-container">
+                            <div class="final-radios-types">
+                                <div class="final-radios-types_slot">
+                                    <label class="custom-radio-container one-logo">Przelew tradycyjny
+                                        <input type="radio" name="radio2" class="custom_input">
+                                        <span class="custom-radio-checkmark"></span>
+                                    </label>
+                                    <div class="pay-images">
+                                        <img src="./assets/icons/cart/wallet2.svg" alt="pay-icon" width="24" height="24">
+                                    </div>
+                                </div>
+                                <div class="final-radios-types_slot">
+                                    <label class="custom-radio-container one-logo">Płatność online
+                                        <input type="radio" name="radio2" class="custom_input">
+                                        <span class="custom-radio-checkmark"></span>
+                                    </label>
+                                    <div class="pay-images">
+                                        <img src="./assets/icons/cart/przelewy24.svg" alt="pay-icon" width="65" height="23">
+                                        <img src="./assets/icons/cart/payu.svg" alt="pay-icon" width="42" height="22">
+                                    </div>
+                                </div>
+                            </div>
+                            <label class="custom-checkbox-container">Akceptuję regulaminy
+                                <input type="checkbox" class="custom_input">
+                                <span class="custom-checkbox-checkmark"></span>
+                            </label>
+                            <p class="desc">Oświadczam, że zapoznałem się z <a href="#">Regulaminem sklepu</a> internetowego macma.pl oraz <a href="#">Regulaminem reklamacji i zwrotów</a>, ich treść jest dla mnie zrozumiała i w całości je akceptuję.</p>
+                            <button class="orange-btn">Zapłać <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></button>
+                            <p class="desc pay-info">Zamówienie z obowiązkiem zapłaty</p>
+                        </div>
+                    </div>
+                </form>
                 <div class="bottom-cart-box-with-btn">
                     <div class="bottom-cart-button">
                         <a href="#" class="white-btn prev left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
