fbd8fa3279c29fe75baa10098f41b18d06b6dee1
goldipl <mgodlewski92@gmail.com>
2024-01-04 14:10:27 +0100

    fill & styling contact page

--- components/contact/contact_content.php
+++ components/contact/contact_content.php
@@ -7,7 +7,22 @@
                 <p>Nasze Dane</p>
             </div>
             <div class="contact-wrapper_col__content">
-                
+                <div class="data-box location">
+                    <b>Macma Polska Sp. z o.o.</b>
+                    <p>Byków, ul. Wrocławska 41</p>
+                    <p>55-095 Mirków k/Wrocławia</p>
+                </div>
+                <div class="data-box data">
+                    <p>NIP: PL8951782271</p>
+                    <p>REGON 932846103</p>
+                    <p>Sąd Rejonowy dla Wrocławia - Fabrycznej we</p>
+                    <p>Wrocławiu, VI Wydział Gospodarczy </p>
+                    <p>KRS 0000144153</p>
+                    <p>Kapitał zakładowy (wpłacony) 850 000 PLN</p>
+                </div>
+            </div>
+            <div class="contact-wrapper_col__content map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20010.49034802634!2d17.1486478!3d51.1764852!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fef577e957a8d%3A0x1a5d684335f8a7d7!2sMacma%20Polska%20Sp.%20Z%20O.o.!5e0!3m2!1spl!2spl!4v1704371760457!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="contact-wrapper_col">
@@ -15,7 +30,26 @@
                 <p>Kontakt</p>
             </div>
             <div class="contact-wrapper_col__content">
-                
+                <div class="contact-box">
+                    <h3 class="office">Biuro</h3>
+                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                    <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+                    <p class="hours">Biuro czynne: poniedziałek-piątek: 8:00-16:00</p>
+                </div>
+                <div class="horizontal-line"></div>
+                <div class="contact-box">
+                    <h3 class="print">Drukarnia</h3>
+                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 397 87 00">+48 71 397 87 00</a></p>
+                    <p class="mail"><a class="gtm-mail" href="mailto:drukarnia@macma.pl">drukarnia@macma.pl</a></p>
+                    <p class="hours">Drukarnia czynna: poniedziałek-piątek: 6:00-14:00</p>
+                </div>
+                <div class="horizontal-line"></div>
+                <div class="contact-box">
+                    <h3 class="storage">Magazyn</h3>
+                    <p class="phone"><a class="gtm-phone" href="tel:+48 71 397 87 18">+48 71 397 87 18</a></p>
+                    <p class="mail"><a class="gtm-mail" href="mailto:magazyn2@macma.pl">magazyn2@macma.pl</a></p>
+                    <p class="hours">Magazyn czynny: poniedziałek-piątek: 8:00-16:00</p>
+                </div>
             </div>
         </div>
         <div class="contact-wrapper_col">
@@ -23,7 +57,37 @@
                 <p>Napisz do Nas</p>
             </div>
             <div class="contact-wrapper_col__content">
-
+                <form class="send-form">
+                    <div class="input-slot">
+                        <label for="name-surname">Imię i nazwisko</label>
+                        <input class="form-input" type="text" name="name-surname" id="name-surname" required>
+                    </div>
+                    <div class="input-slot">
+                        <label for="company">Firma</label>
+                        <input class="form-input" type="text" name="company" id="company">
+                    </div>
+                    <div class="input-slot">
+                        <label for="nip-code">NIP</label>
+                        <input class="form-input" type="text" name="nip-code" id="nip-code">
+                    </div>
+                    <div class="input-slot">
+                        <label for="email">Email</label>
+                        <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email">
+                    </div>
+                    <div class="input-slot">
+                        <label for="telephone">Telefon</label>
+                        <input class="form-input" type="text" name="telephone" id="telephone" required>
+                    </div>
+                    <div class="input-slot">
+                        <label for="msgarea">Wiadomość</label>
+                        <textarea class="form-textarea" name="msgarea" id="msgarea" required></textarea>
+                    </div>
+                    <div class="checkbox-button-box">
+                        <label class="custom-checkbox-container">Administratorem Państwa danych osobowych jest Macma Polska Sp. z o.o. (NIP: 8951782271). Dane osobowe przetwarzane będą wyłącznie w prawnie usprawiedliwionych celach administratora danych polegających na udzieleniu odpowiedzi na Państwa wiadomość. Podanie przez Państwa danych osobowych jest dobrowolne, ale też niezbędne do nawiązania z Państwem kontaktu za pomocą formularza kontaktowego. Szczegółowe informacje na temat przetwarzania Państwa danych osobowych mogą Państwo znaleźć w naszej <a href="#">Polityce prywatności</a> na temat zasad przetwarzania danych osobowych.<input type="checkbox" class="custom_input"><span class="custom-checkbox-checkmark"></span>
+                        </label>
+                        <button class="orange-btn">Wyślij <img src="./assets/icons/common/buttons/button_white_arrow.svg"></button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
