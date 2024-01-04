84f18925783cb1ca635f32bd8ea580b4bcc190b7
goldipl <mgodlewski92@gmail.com>
2024-01-04 12:13:22 +0100

    add contact page starter php & scss files

---
+++ components/contact/contact_content.php
@@ -0,0 +1,30 @@
+<div class="contact-container container">
+    <h1>Kontakt</h1>
+    <h4>Jesteśmy dystrybutorem gadżetów reklamowych</h4>
+    <div class="contact-wrapper">
+        <div class="contact-wrapper_col">
+            <div class="title">
+                <p>Nasze Dane</p>
+            </div>
+            <div class="contact-wrapper_col__content">
+                
+            </div>
+        </div>
+        <div class="contact-wrapper_col">
+            <div class="title">
+                <p>Kontakt</p>
+            </div>
+            <div class="contact-wrapper_col__content">
+                
+            </div>
+        </div>
+        <div class="contact-wrapper_col">
+            <div class="title">
+                <p>Napisz do Nas</p>
+            </div>
+            <div class="contact-wrapper_col__content">
+
+            </div>
+        </div>
+    </div>
+</div>
