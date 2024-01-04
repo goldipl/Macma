9a1d81b136b2da80c055d5fd01fa25bf00678efd
goldipl <mgodlewski92@gmail.com>
2024-01-04 15:45:57 +0100

    fill & styling logged contact template

---
+++ components/contact/contact_other_departments_content.php
@@ -0,0 +1,31 @@
+<div class="other-department-box container">
+    <div class="other-department-box__title">
+        <p>Pozostałe działy</p>
+    </div>
+    <div class="other-department-box__row">
+        <div class="other-department-box__row__col">
+            <b class="graphic-department">Dział Graficzny</b>
+            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+        </div>
+        <div class="other-department-box__row__col">
+            <b class="accountancy">Księgowość</b>
+            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+        </div>
+        <div class="other-department-box__row__col">
+            <b class="marketing-department">Dział Marketingu</b>
+            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+        </div>
+        <div class="other-department-box__row__col">
+            <b class="import-department">Dział Importu</b>
+            <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+            <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+        </div>
+    </div>
+</div>
---
+++ components/contact/contact_sales_department_content.php
@@ -0,0 +1,97 @@
+<div class="sales-department-box container">
+    <div class="sales-department-box__title">
+        <p>Nasz dział handlowy</p>
+    </div>
+    <div class="sales-department-box__row">
+        <div class="sales-department-box__row__col">
+            <div class="sales-department-box__row__col__top">
+                <img src="./assets/img/contact/person01.jpg" alt="person img" width="248" height="173">
+            </div>
+            <div class="sales-department-box__row__col__middle">
+                <p>Jan Nowak</p>
+                <span>Dział handlowy | Stanowisko</span>
+            </div>
+            <div class="horizontal-line"></div>
+            <div class="sales-department-box__row__col__bottom">
+                <p class="tel1 gtm-phone"><a href="tel:17 555 41 65">17 555 41 65</a></p>
+                <p class="tel2 gtm-phone"><a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                <p class="mail gtm-mail"><a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            </div>
+        </div>
+        <div class="sales-department-box__row__col">
+            <div class="sales-department-box__row__col__top">
+                <img src="./assets/img/contact/person02.jpg" alt="person img" width="248" height="173">
+            </div>
+            <div class="sales-department-box__row__col__middle">
+                <p>Jan Nowak</p>
+                <span>Dział handlowy | Stanowisko</span>
+            </div>
+            <div class="horizontal-line"></div>
+            <div class="sales-department-box__row__col__bottom">
+                <p class="tel1 gtm-phone"><a href="tel:17 555 41 65">17 555 41 65</a></p>
+                <p class="tel2 gtm-phone"><a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                <p class="mail gtm-mail"><a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            </div>
+        </div>
+        <div class="sales-department-box__row__col">
+            <div class="sales-department-box__row__col__top">
+                <img src="./assets/img/contact/person03.jpg" alt="person img" width="248" height="173">
+            </div>
+            <div class="sales-department-box__row__col__middle">
+                <p>Jan Nowak</p>
+                <span>Dział handlowy | Stanowisko</span>
+            </div>
+            <div class="horizontal-line"></div>
+            <div class="sales-department-box__row__col__bottom">
+                <p class="tel1 gtm-phone"><a href="tel:17 555 41 65">17 555 41 65</a></p>
+                <p class="tel2 gtm-phone"><a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                <p class="mail gtm-mail"><a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            </div>
+        </div>
+        <div class="sales-department-box__row__col">
+            <div class="sales-department-box__row__col__top">
+                <img src="./assets/img/contact/person04.jpg" alt="person img" width="248" height="173">
+            </div>
+            <div class="sales-department-box__row__col__middle">
+                <p>Jan Nowak</p>
+                <span>Dział handlowy | Stanowisko</span>
+            </div>
+            <div class="horizontal-line"></div>
+            <div class="sales-department-box__row__col__bottom">
+                <p class="tel1 gtm-phone"><a href="tel:17 555 41 65">17 555 41 65</a></p>
+                <p class="tel2 gtm-phone"><a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                <p class="mail gtm-mail"><a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            </div>
+        </div>
+        <div class="sales-department-box__row__col">
+            <div class="sales-department-box__row__col__top">
+                <img src="./assets/img/contact/person05.jpg" alt="person img" width="248" height="173">
+            </div>
+            <div class="sales-department-box__row__col__middle">
+                <p>Jan Nowak</p>
+                <span>Dział handlowy | Stanowisko</span>
+            </div>
+            <div class="horizontal-line"></div>
+            <div class="sales-department-box__row__col__bottom">
+                <p class="tel1 gtm-phone"><a href="tel:17 555 41 65">17 555 41 65</a></p>
+                <p class="tel2 gtm-phone"><a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                <p class="mail gtm-mail"><a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            </div>
+        </div>
+        <div class="sales-department-box__row__col">
+            <div class="sales-department-box__row__col__top">
+                <img src="./assets/img/contact/person06.jpg" alt="person img" width="248" height="173">
+            </div>
+            <div class="sales-department-box__row__col__middle">
+                <p>Jan Nowak</p>
+                <span>Dział handlowy | Stanowisko</span>
+            </div>
+            <div class="horizontal-line"></div>
+            <div class="sales-department-box__row__col__bottom">
+                <p class="tel1 gtm-phone"><a href="tel:17 555 41 65">17 555 41 65</a></p>
+                <p class="tel2 gtm-phone"><a href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
+                <p class="mail gtm-mail"><a href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
+            </div>
+        </div>
+    </div>
+</div>
