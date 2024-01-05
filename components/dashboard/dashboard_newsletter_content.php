c00d4005c88f96cb442957f69c860079e3922e88
goldipl <mgodlewski92@gmail.com>
2024-01-05 16:45:37 +0100

    add dashboard newsletter page with styles

---
+++ components/dashboard/dashboard_newsletter_content.php
@@ -0,0 +1,12 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/profile.svg" alt="menu icon" width="20" height="20">Twój newsletter</p>
+    </div>
+    <div class="inputs-wrapper">
+        <div class="inputs-wrapper__slot newsletter">
+            <label for="newslettermail">Twój adres email<span class="orange">*</span></label>
+            <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="newslettermail" id="newslettermail" required>
+        </div>
+        <button class="orange-btn">Zapisz się <img src="./assets/icons/dashboard/save-btn.svg" alt="save btn icon" width="18" height="18"></button>
+    </div>
+</form>
