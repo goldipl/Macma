c90ad15ed719809c0bc9a973ea189e99a3c9408d
goldipl <mgodlewski92@gmail.com>
2024-01-05 09:39:07 +0100

    fill template & styling dashboard menu

--- components/dashboard/dashboard_menu.php
+++ components/dashboard/dashboard_menu.php
@@ -2,4 +2,42 @@
     <div class="header">
         <p>Menu</p>
     </div>
+    <div class="menu-links-container">
+        <div class="menu-links-box">
+            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/profile.svg" alt="menu icon" width="20" height="20">Profil</a>
+            <a class="menu-link" href="#">Twoje dane</a>
+            <a class="menu-link" href="#">Newsletter</a>
+            <a class="menu-link" href="#">Program Rozwoju</a>
+        </div>
+        <div class="menu-links-box">
+            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Zamówienia</a>
+            <a class="menu-link" href="#">Zamówienia</a>
+            <a class="menu-link" href="#">FV i listy przewozowe</a>
+            <a class="menu-link" href="#">Adresy dostaw</a>
+            <a class="menu-link" href="#">Wizualizacje</a>
+            <a class="menu-link" href="#">Logotypy</a>
+        </div>
+        <div class="menu-links-box">
+            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/offerts.svg" alt="menu icon" width="20" height="20">Oferty</a>
+            <a class="menu-link" href="#">Oferty handlowe</a>
+            <a class="menu-link" href="#">Twój ofertownik</a>
+            <a class="menu-link" href="#">Kalkulacje</a>
+            <a class="menu-link" href="#">Negocjacje</a>
+        </div>
+        <div class="menu-links-box">
+            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/to_download.svg" alt="menu icon" width="20" height="20">Do pobrania</a>
+            <a class="menu-link" href="#">Cenniki</a>
+            <a class="menu-link" href="#">Katalogi</a>
+            <a class="menu-link" href="#">Newslettery</a>
+            <a class="menu-link" href="#">Video</a>
+            <a class="menu-link" href="#">Materiały marketingowe</a>
+        </div>
+        <div class="menu-links-box">
+            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/api.svg" alt="menu icon" width="20" height="20">Web API</a>
+        </div>
+        <div class="menu-links-box">
+            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/cart.svg" alt="menu icon" width="20" height="20">Sklep No-Name</a>
+        </div>
+        <button class="logout">Wyloguj się</button>
+    </div>
 </aside>
