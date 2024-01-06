fc8c7bbcf65dc2fc3ee7c339a893acc2343f8198
goldipl <mgodlewski92@gmail.com>
2024-01-06 10:57:25 +0100

    fill templates & styling dashboard program page

---
+++ components/dashboard/dashboard_program_content.php
@@ -0,0 +1,60 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/profile.svg" alt="menu icon" width="20" height="20">Twój program rozwoju</p>
+    </div>
+    <div class="dashboard-program">
+        <h2>Gratulacje</h2>
+        <img src="./assets/img/dashboard/macma-gold-status.jpg" alt="Macma lvl img" width="500" height="200">
+        <p>Do osiągnięcia kolejnego poziomu brakuje Ci <span>XXX</span> zł obrotu.</p>
+    </div>
+    <div class="progress-box">
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-label="Basic example" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/profile.svg" alt="menu icon" width="20" height="20">Korzyści z programu</p>
+    </div>
+    <div class="program-benefits-box">
+        <div class="program-benefits-box__left">
+            <div class="benefit-row-box">
+                <div class="benefit-row-box__left">
+                    <p>Darmowe dostawy od kwoty:</p>
+                </div>
+                <div class="benefit-row-box__right">
+                    <p class="green">750 zł</p>
+                </div>
+            </div>
+            <div class="benefit-row-box">
+                <div class="benefit-row-box__left">
+                    <p>Poniżej 750 PLN netto koszt transportu w 1 miejsce:</p>
+                </div>
+                <div class="benefit-row-box__right">
+                    <p class="green">16 zł</p>
+                </div>
+            </div>
+            <div class="benefit-row-box">
+                <div class="benefit-row-box__left">
+                    <p>Wysyłka do 14:00 dla towarów bez znakowania:</p>
+                </div>
+                <div class="benefit-row-box__right">
+                    <p class="green">Tak</p>
+                </div>
+            </div>
+            <div class="benefit-row-box">
+                <div class="benefit-row-box__left">
+                    <p>Gwarantowany termin znakowania:</p>
+                    <p class="desc">*nie dotyczy kalki ceramicznej oraz tłoczenia</p>
+                </div>
+                <div class="benefit-row-box__right">
+                    <p class="green">24 h</p>
+                </div>
+            </div>
+        </div>
+        <div class="program-benefits-box__right">
+            <p>Sprawdź Program Rozwoju</p>
+            <img src="./assets/img/dashboard/macma-arrows-grey.jpg" alt="Program Rozwoju" width="500" height="110">
+            <a class="download-leaflet-btn" href="#">Pobierz ulotkę</a>
+        </div>
+    </div>
+</form>
