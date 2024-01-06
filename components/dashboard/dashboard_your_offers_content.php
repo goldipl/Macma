7ba0b87d7769ab5b13a7df1591a40c94557913da
goldipl <mgodlewski92@gmail.com>
2024-01-06 13:59:16 +0100

    add your offerts template page

---
+++ components/dashboard/dashboard_your_offers_content.php
@@ -0,0 +1,77 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/offerts.svg" alt="menu icon" width="20" height="20">Twój ofertownik</p>
+    </div>
+    <div class="dashboard-your-offers dashboard-table">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Nazwa</th>
+                    <th>Produkty</th>
+                    <th>Do koszyka</th>
+                    <th>Usuń</th>
+                </tr>
+                <tr class="spacer15"></tr>
+                <tr>
+                    <td>Oferta nr 1, 2023-12-12</td>
+                    <td>3</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>Oferta nr 2, 2023-12-12</td>
+                    <td>3</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>Oferta nr 3, 2023-12-12</td>
+                    <td>4</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>Oferta nr 4, 2023-12-12</td>
+                    <td>2</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</form>
