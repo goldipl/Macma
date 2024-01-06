4c6401b88f8cee3306d748df9f3eee24b70b150f
goldipl <mgodlewski92@gmail.com>
2024-01-06 14:55:13 +0100

    add dashboard negotiations page

---
+++ components/dashboard/dashboard_negotiations_content.php
@@ -0,0 +1,118 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/offerts.svg" alt="menu icon" width="20" height="20">Twoje Negocjacje</p>
+    </div>
+    <div class="dashboard-negotiations dashboard-table">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Data</th>
+                    <th>Produkty</th>
+                    <th>Do koszyka</th>
+                    <th>Negocjuj ponownie</th>
+                    <th>Usuń</th>
+                </tr>
+                <tr class="spacer15"></tr>
+                <tr>
+                    <td>2023-08-09</td>
+                    <td>Torba plażowa Sao Luís (363213), 100 szt.</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="negotiate">
+                            <img src="./assets/icons/dashboard/money.svg" alt="negotiate icon" width="22" height="17">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-08-09</td>
+                    <td>Torba plażowa Sao Luís (363213), 100 szt.</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="negotiate">
+                            <img src="./assets/icons/dashboard/money.svg" alt="negotiate icon" width="22" height="17">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-08-09</td>
+                    <td>Torba plażowa Sao Luís (363213), 100 szt.</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="negotiate">
+                            <img src="./assets/icons/dashboard/money.svg" alt="negotiate icon" width="22" height="17">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-08-09</td>
+                    <td>Torba plażowa Sao Luís (363213), 100 szt.</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="negotiate">
+                            <img src="./assets/icons/dashboard/money.svg" alt="negotiate icon" width="22" height="17">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-08-09</td>
+                    <td>Torba plażowa Sao Luís (363213), 100 szt.</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="negotiate">
+                            <img src="./assets/icons/dashboard/money.svg" alt="negotiate icon" width="22" height="17">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</form>
