7bb7329809ffebedbaee1b75ba227298a464b42c
goldipl <mgodlewski92@gmail.com>
2024-01-06 15:02:31 +0100

    add price lists dashboard

---
+++ components/dashboard/dashboard_pricelists_content.php
@@ -0,0 +1,20 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/to_download.svg" alt="menu icon" width="20" height="20">Cenniki</p>
+    </div>
+    <div class="dashboard-pricelists dashboard-table">
+        <table>
+            <tbody>
+                <tr>
+                    <td><a href="#">Cennik znakowania 2023</a></td>
+                    <td>XLSX, 32.98 KB</td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td><a href="#">Cennik znakowania 2023 PDF</a></td>
+                    <td>PDF, 688.3 KB</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</form>
