c361ac30063eefd7b6a1d5141a590d55be080729
goldipl <mgodlewski92@gmail.com>
2024-01-06 12:50:12 +0100

    fill & styling delivery addresses dashboard page

---
+++ components/dashboard/dashboard_delivery_addresses_content.php
@@ -0,0 +1,99 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Twoje Adresy Dostaw</p>
+    </div>
+    <div class="dashboard-delivery-addresses">
+        <div class="dashboard-delivery-addresses__slot">
+            <div class="delivery-box-content">
+                <h4>Adres</h4>
+                <p>Firma 1</p>
+                <p>Krakowska, 4c/14</p>
+                <p>35-582 Kraków</p>
+                <p>Mirosław Kozłowski</p>
+                <p>657 557 2526</p>
+                <span class="default-address"><img src="./assets/icons/dashboard/checked_orange.svg" alt="menu icon" width="18" height="18">Domyślny adres odbiorcy</span>
+            </div>
+            <div class="delivery-box-bottom">
+                <div class="bottom-icons">
+                    <a class="edit" href="#">
+                        <img src="./assets/icons/dashboard/edit.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/delete.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-delivery-addresses__slot">
+            <div class="delivery-box-content">
+                <h4>Adres</h4>
+                <p>Firma 1</p>
+                <p>Krakowska, 4c/14</p>
+                <p>35-582 Kraków</p>
+                <p>Mirosław Kozłowski</p>
+                <p>657 557 2526</p>
+                <span class="default-address"></span>
+            </div>
+            <div class="delivery-box-bottom">
+                <div class="bottom-icons">
+                    <a class="edit" href="#">
+                        <img src="./assets/icons/dashboard/edit.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/delete.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-delivery-addresses__slot">
+            <div class="delivery-box-content">
+                <h4>Adres</h4>
+                <p>Firma 1</p>
+                <p>Krakowska, 4c/14</p>
+                <p>35-582 Kraków</p>
+                <p>Mirosław Kozłowski</p>
+                <p>657 557 2526</p>
+                <span class="default-address"></span>
+            </div>
+            <div class="delivery-box-bottom">
+                <div class="bottom-icons">
+                    <a class="edit" href="#">
+                        <img src="./assets/icons/dashboard/edit.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/delete.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-delivery-addresses__slot">
+            <div class="delivery-box-content">
+                <h4>Adres</h4>
+                <p>Firma 1</p>
+                <p>Krakowska, 4c/14</p>
+                <p>35-582 Kraków</p>
+                <p>Mirosław Kozłowski</p>
+                <p>657 557 2526</p>
+                <span class="default-address"></span>
+            </div>
+            <div class="delivery-box-bottom">
+                <div class="bottom-icons">
+                    <a class="edit" href="#">
+                        <img src="./assets/icons/dashboard/edit.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/delete.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-delivery-addresses__slot">
+            <div class="delivery-box-content add-address">
+                <div class="add-address-box">
+                    <img src="./assets/icons/dashboard/add_orange.svg" alt="menu icon" width="18" height="18">
+                    <span>Dodaj Adres</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
