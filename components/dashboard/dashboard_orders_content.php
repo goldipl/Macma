b208a481c749a5fdf35102a2ff40673f32f0cde5
goldipl <mgodlewski92@gmail.com>
2024-01-06 11:26:32 +0100

    fill & styling dashboard orders

---
+++ components/dashboard/dashboard_orders_content.php
@@ -0,0 +1,41 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Twoje zamówienia</p>
+    </div>
+    <div class="dashboard-orders">
+        <div class="dashboard-orders__month">
+            <p>Sierpień 2023</p>
+        </div>
+        <div class="dashboard-orders__slot">
+            <div class="dashboard-orders__slot--left">
+                <img src="./assets/img/dashboard/product01.jpg" alt="product img" width="128" height="128">
+            </div>
+            <div class="dashboard-orders__slot--middle">
+                <p class="product-title">Opaska na rękę Mansfield</p>
+                <p>Nr zamówienia: 700000703086</p>
+                <p>Data: Śr, 05.07.2023</p>
+            </div>
+            <div class="dashboard-orders__slot--right">
+                <p class="status">Status:<span class="in-progress">W realizacji</span></p>
+                <a class="dashboard-product-details-btn" href="#">Szczegóły</a>
+            </div>
+        </div>
+        <div class="dashboard-orders__month">
+            <p>Czerwiec 2023</p>
+        </div>
+        <div class="dashboard-orders__slot">
+            <div class="dashboard-orders__slot--left">
+                <img src="./assets/img/dashboard/product02.jpg" alt="product img" width="128" height="128">
+            </div>
+            <div class="dashboard-orders__slot--middle">
+                <p class="product-title">Opaska na rękę Mansfield</p>
+                <p>Nr zamówienia: 700000703086</p>
+                <p>Data: Śr, 05.07.2023</p>
+            </div>
+            <div class="dashboard-orders__slot--right">
+                <p class="status">Status:<span class="send">Wysłane</span></p>
+                <a class="dashboard-product-details-btn" href="#">Szczegóły</a>
+            </div>
+        </div>
+    </div>
+</form>
---
+++ dashboard-orders.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MACMA - Panel Klienta - Zamówienia</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/select2.min.css" />
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body>
+        <header> 
+            <?php include "./components/common/topbar.php"; ?> 
+            <?php include "./components/common/nav.php"; ?> 
+        </header>
+        <main id="main-wrapper">
+            <div class="dashboard-container container">
+                <div class="dashboard-container__left">
+                    <?php include "./components/dashboard/dashboard_menu.php"; ?> 
+                </div>
+                <div class="dashboard-container__right">
+                    <?php include "./components/dashboard/dashboard_title.php"; ?>
+                    <?php include "./components/dashboard/dashboard_orders_content.php"; ?> 
+                </div>
+            </div>
+        </main>
+        <footer> 
+            <?php include "./components/common/footer.php"; ?> 
+        </footer>
+        <script>
+            document.querySelector('.menu-links-box.big-orders > a:nth-of-type(2)').classList.add('active');
+        </script>
+        <script src="./js/jquery.min.js"></script>
+        <script src="./js/popper.min.js"></script>
+        <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="./js/bootbox.min.js"></script>
+        <script src="./js/select2.min.js"></script>
+        <script src="./js/script.js"></script>
+        <script src="./js/dashboard/dashboard_menu.js"></script>
+        <script src="./js/dashboard/upload_logo.js"></script>
+    </body>
+</html>
