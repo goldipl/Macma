3aedd534e4398d5522fd45a2378cf5a55586087b
goldipl <mgodlewski92@gmail.com>
2024-01-06 13:33:04 +0100

    fill dashboard logos templates & styles

---
+++ components/dashboard/dashboard_logos_content.php
@@ -0,0 +1,48 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Twoje logotypy</p>
+    </div>
+    <div class="dashboard-logos">
+        <div class="dashboard-logos__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/logo01.jpg" alt="logo img" width="180" height="180">
+            </div>
+            <div class="product-content">
+                <p>Logo</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/delete.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-logos__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/logo02.jpg" alt="logo img" width="180" height="180">
+            </div>
+            <div class="product-content">
+                <p>Logo</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/delete.svg" alt="menu icon" width="17" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-logos__slot">
+            <div class="add-file-box">
+                <label for="files" class="upload-files-box">
+                    <img src="./assets/icons/upload-icon.svg" alt="upload icon" width="21" height="14">
+                    <p>Przeciągnij i upuść albo<br><span>Dodaj plik</span></p>
+                    <input class="file file-block" type="file" id="files-logo">
+                </label>               
+            </div>
+        </div>
+    </div>
+</form>
