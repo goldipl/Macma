55f3ae1e69dd1067059393ba98d5980779b36220
goldipl <mgodlewski92@gmail.com>
2024-01-06 14:45:56 +0100

    fill & style your calculations template

---
+++ components/dashboard/dashboard_calculations_content.php
@@ -0,0 +1,102 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/offerts.svg" alt="menu icon" width="20" height="20">Twój ofertownik</p>
+    </div>
+    <div class="dashboard-calculations dashboard-table">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Data</th>
+                    <th>Nazwa</th>
+                    <th>Cena</th>
+                    <th>Do koszyka</th>
+                    <th>Usuń</th>
+                </tr>
+                <tr class="spacer15"></tr>
+                <tr>
+                    <td>2023-08-09</td>
+                    <td>
+                        <div class="info-desc">
+                            <p>Torba bawełniana MANACOR (216406), 100 szt.</p>
+                            <p class="desc">Znakowanie: ST1 (sitodruk na tekstyliach - białe/natural), Kolory: 1, Miejsca: 1Znakowanie: DTG1 (druk DTG - torby bawełniane 30x30cm), Kolory: 1, Miejsca: 1</pn>
+                        </div>
+                    </td>
+                    <td>1259,00 PLN</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-12-09</td>
+                    <td>
+                        <div class="info-desc">
+                            <p>Torba bawełniana MANACOR (216406), 1000 szt.</p>
+                            <p class="desc">Znakowanie: ST1 (sitodruk na tekstyliach - białe/natural), Kolory: 1, Miejsca: 1Znakowanie: DTG1 (druk DTG - torby bawełniane 30x30cm), Kolory: 1, Miejsca: 1</pn>
+                        </div>
+                    </td>
+                    <td>12 590,00 PLN</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-05-05</td>
+                    <td>
+                        <div class="info-desc">
+                            <p>Torba bawełniana MANACOR (216406), 150 szt.</p>
+                            <p class="desc">Znakowanie: ST1 (sitodruk na tekstyliach - białe/natural), Kolory: 1, Miejsca: 1Znakowanie: DTG1 (druk DTG - torby bawełniane 30x30cm), Kolory: 1, Miejsca: 4</pn>
+                        </div>
+                    </td>
+                    <td>1259,00 PLN</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>2023-10-09</td>
+                    <td>
+                        <div class="info-desc">
+                            <p>Torba bawełniana MANACOR (216406), 200 szt.</p>
+                            <p class="desc">Znakowanie: ST1 (sitodruk na tekstyliach - białe/natural), Kolory: 1, Miejsca: 1Znakowanie: DTG1 (druk DTG - torby bawełniane 30x30cm), Kolory: 1, Miejsca: 1</pn>
+                        </div>
+                    </td>
+                    <td>1259,00 PLN</td>
+                    <td>
+                        <a href="#" class="cart">
+                            <img src="./assets/icons/dashboard/cart.svg" alt="cart icon" width="20" height="20">
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#" class="delete">
+                            <img src="./assets/icons/dashboard/delete.svg" alt="delete icon" width="15" height="17">
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</form>
