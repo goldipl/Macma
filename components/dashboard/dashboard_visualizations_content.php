8fa86d6d61b0e4f4c7f3fcc52deec667953f961f
goldipl <mgodlewski92@gmail.com>
2024-01-06 13:09:47 +0100

    fill & styling dashboard visualizations page

---
+++ components/dashboard/dashboard_visualizations_content.php
@@ -0,0 +1,58 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Twoje WIZUALIZACJE<span class="orange">Aktualizacja plików codziennie po godzinie 17:00.</span></p>
+    </div>
+    <div class="dashboard-visualizations">
+        <div class="dashboard-visualizations__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/product03.jpg" alt="product img" width="196" height="196">
+            </div>
+            <div class="product-content">
+                <div class="product-content__top">
+                    <p>Długopis metalowy ABU DHABI</p>
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                </div>
+                <div class="product-content__bottom">
+                    <p>data aktualizacji:</p>
+                    <p>08.06.2023</p>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-visualizations__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/product04.jpg" alt="product img" width="196" height="196">
+            </div>
+            <div class="product-content">
+                <div class="product-content__top">
+                    <p>Otwieracz do butelek plastikowy LUTON</p>
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                </div>
+                <div class="product-content__bottom">
+                    <p>data aktualizacji:</p>
+                    <p>08.06.2023</p>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-visualizations__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/product05.jpg" alt="product img" width="196" height="196">
+            </div>
+            <div class="product-content">
+                <div class="product-content__top">
+                    <p>Pokrowiec na kartę do smartfona BORDEAUX</p>
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                </div>
+                <div class="product-content__bottom">
+                    <p>data aktualizacji:</p>
+                    <p>08.06.2023</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
