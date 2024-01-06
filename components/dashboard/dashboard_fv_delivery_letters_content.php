3b2af50c0e41f35c5fb609af2235bcd6d779a9c9
goldipl <mgodlewski92@gmail.com>
2024-01-06 12:01:10 +0100

    fill & styling fv & delivery letters page

---
+++ components/dashboard/dashboard_fv_delivery_letters_content.php
@@ -0,0 +1,62 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Twoje Faktury i LISTY PRZEWOZOWE</p>
+    </div>
+    <div class="dashboard-fv-delivery-letters">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Data</th>
+                    <th>Nazwa</th>
+                    <th>Zamawiający</th>
+                    <th>Kwota</th>
+                    <th>Status</th>
+                    <th>Pobierz</th>
+                    <th>Nr przesyłki</th>
+                </tr>
+                <tr class="spacer15"></tr>
+                <tr>
+                    <td>08.08.2023</td>
+                    <td>FV nr 34/8</td>
+                    <td>Emanuel Nowak</td>
+                    <td>1 900 zł</td>
+                    <td class="status green">opłacona</td>
+                    <td class="download-icon">
+                        <a href="#">
+                            <img src="./assets/icons/dashboard/download.svg" alt="download icon" width="21" height="17">
+                        </a>
+                    </td>
+                    <td class="delivery-number">43241560W6840931356</td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>08.08.2023</td>
+                    <td>FV nr 34/8</td>
+                    <td>Maciej Jaworski</td>
+                    <td>1 900 zł</td>
+                    <td class="status orange">do zapłaty</td>
+                    <td class="download-icon">
+                        <a href="#">
+                            <img src="./assets/icons/dashboard/download.svg" alt="download icon" width="21" height="17">
+                        </a>
+                    </td>
+                    <td class="delivery-number">432553543534543543356</td>
+                </tr>
+                <tr class="spacer10"></tr>
+                <tr>
+                    <td>08.08.2023</td>
+                    <td>FV nr 34/8</td>
+                    <td>Amadeusz Sawicki</td>
+                    <td>1 900 zł</td>
+                    <td class="status red">przeterminowana</td>
+                    <td class="download-icon">
+                        <a href="#">
+                            <img src="./assets/icons/dashboard/download.svg" alt="download icon" width="21" height="17">
+                        </a>
+                    </td>
+                    <td class="delivery-number">4324156065460931356</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</form>
