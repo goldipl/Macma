eb5d02599c480b4b5de8105586a18fbc763fabbb
goldipl <mgodlewski92@gmail.com>
2024-01-07 11:21:27 +0100

    add marketing materials dashboard page

---
+++ components/dashboard/dashboard_marketing_materials_content.php
@@ -0,0 +1,20 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Materiały marketingowe</p>
+    </div>
+    <div class="dashboard-img-boxes">
+        <div class="dashboard-img-boxes__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/marketing01.jpg" alt="marketing materials img" width="180" height="180">
+            </div>
+            <div class="product-content">
+                <p>Logo</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
