a52ee5126817ff6812946ae49ad7058b02d0eac4
goldipl <mgodlewski92@gmail.com>
2024-01-07 12:29:00 +0100

    styling dahboard webapi

--- components/dashboard/dashboard_webapi_content.php
+++ components/dashboard/dashboard_webapi_content.php
@@ -3,6 +3,14 @@
         <p><img src="./assets/icons/dashboard/api.svg" alt="menu icon" width="25" height="25">Webapi</p>
     </div>
     <div class="dashboard-text-img-content">
-
+        <div class="dashboard-text-img-content__text">
+            <p>Zachęcamy do korzystania z systemu <strong>WEBAPI</strong> dostępnego pod adresem </p>
+            <p>Znajdują się tam pliki zawierające wszystkie najważniejsze informacje dotyczące naszej oferty, które mogą być wykorzystywane na Państwa stronach i w sklepach internetowych. </p>
+            <p>Pliki do pobrania w formatach: <strong>XML, CSV, JSON, TXT</strong>. Informacje aktualizowane są automatycznie. Dane do logowania są takie same jak do logowania na stronie</p>
+            <a class="downl-btn" href="">Pobierz instrukcję PDF</a>
+        </div>
+        <div class="dashboard-text-img-content__img">
+            <img src="./assets/img/dashboard/api.jpg" alt="API img" width="518" height="320">
+        </div>
     </div>
 </form>
