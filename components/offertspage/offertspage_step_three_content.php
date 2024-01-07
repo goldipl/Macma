2009dcede3c213872a569dccb9754be4c24570cf
goldipl <mgodlewski92@gmail.com>
2024-01-07 18:49:08 +0100

    add offertspage step three with styles

--- components/offertspage/offertspage_step_three_content.php
+++ components/offertspage/offertspage_step_three_content.php
@@ -21,12 +21,14 @@
                                 <th>Produkt</th>
                                 <th>Ilość</th>
                                 <th>Cena<br>katalogowa</th>
-                                <th>Rabat</th>
                                 <th>Twoja cena<br>netto szt.</th>
                                 <th>Wartość<br>netto</th>
-                                <th>Wartość<br>brutto</th>
-                                <th>Usuń</th>
+                                <th>Narzut %</th>
+                                <th>Jednostkowa cena<br> dla klienta<br> netto/szt</th>
+                                <th>Wartość<br> dla klienta<br> netto</th>
+                                <th>Zysk</th>
                             </tr>
+                            <!-- Product 01 -->
                             <tr>
                                 <td class="table-number-col"><b>1</b></td>
                                 <td class="table-product-col">
@@ -46,30 +48,78 @@
                                         </div>
                                     </div>
                                 </td>
-                                <td>
-                                    <input type="number" name="product-quantity" id="product-quantity">
-                                </td>
+                                <td>10</td>
                                 <td>1,95 zł</td>
-                                <td>44%</td>
                                 <td>1,09 PLN</td>
                                 <td>1,09 PLN</td>
-                                <td>1,34 PLN</td>
                                 <td>
-                                    <a class="delete-icon" href="#">
-                                        <img src="./assets/icons/offertspage/delete.svg" alt="delete" width="17" height="20">
-                                    </a>
+                                    <input type="number" name="product-mark-up" id="product-mark-up">
+                                </td>
+                                <td>
+                                    <input type="text" name="product-unit-price" id="product-unit-price">
+                                </td>
+                                <td>167,00 PLN</td>
+                                <td class="green">56,00</td>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b></b></td>
+                                <td class="table-product-col">
+                                    <div class="table-product-box">
+                                        <p class="m-0">Pierwszy logotyp:</p>
+                                        <p class="m-0">L0 (grawer laserowy)</p>
+                                    </div>
+                                </td>
+                                <td>10</td>
+                                <td>
+                                    <div class="table-product-box">
+                                        <p class="m-0">Miejsca: 1</p>
+                                        <p class="m-0">Kolory: 1</p>
+                                    </div>
+                                </td>
+                                <td>0,49</td>
+                                <td>49,00</td>
+                                <td>
+                                    <input type="number" name="product-mark-up" id="product-mark-up">
+                                </td>
+                                <td>
+                                    <input type="text" name="product-unit-price" id="product-unit-price">
+                                </td>
+                                <td>167,00 PLN</td>
+                                <td class="green">39,00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="5">
+                                    <div class="bottom-offertspage-box-with-btn">
+                                        <div class="bottom-offertspage-box">
+                                            <p>Wpisz narzut dla wszystkich produktów</p>
+                                            <input class="form-input" type="number" name="all-quantity">
+                                            <button class="confirm-btn">Zatwierdź</button>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td colspan="5">
+                                    <div class="bottom-offertspage-box-with-btn">
+                                        <div class="bottom-offertspage-box">
+                                            <p>Wpisz narzut dla wszystkich pozycji znakowania</p>
+                                            <input class="form-input" type="number" name="all-quantity">
+                                            <button class="confirm-btn">Zatwierdź</button>
+                                        </div>
+                                    </div>
                                 </td>
                             </tr>
+                            <!-- spacer15 -->
+                            <tr class="spacer15"></tr>
+                            <!-- Product 02 -->
                             <tr>
                                 <td class="table-number-col"><b>2</b></td>
                                 <td class="table-product-col">
                                     <div class="table-product-box">
                                         <div class="table-product-box__top">
-                                            <p>Notatnik A5</p>
+                                            <p>Długopis metalowy</p>
                                         </div>
                                         <div class="table-product-box__bottom">
                                             <div class="table-product-box__bottom--left">
-                                                <img src="./assets/img/offertspage/notepad.jpg" alt="img" width="71" height="71">
+                                                <img src="./assets/img/offertspage/pen.jpg" alt="img" width="71" height="71">
                                             </div>
                                             <div class="table-product-box__bottom--right">
                                                 <p>Kod: <span>1333903</span></p>
@@ -79,41 +129,101 @@
                                         </div>
                                     </div>
                                 </td>
-                                <td>
-                                    <input type="number" name="product-quantity" id="product-quantity">
-                                </td>
+                                <td>10</td>
                                 <td>1,95 zł</td>
-                                <td>44%</td>
                                 <td>1,09 PLN</td>
                                 <td>1,09 PLN</td>
-                                <td>1,34 PLN</td>
                                 <td>
-                                    <a class="delete-icon" href="#">
-                                        <img src="./assets/icons/offertspage/delete.svg" alt="delete" width="17" height="20">
-                                    </a>
+                                    <input type="number" name="product-mark-up" id="product-mark-up">
+                                </td>
+                                <td>
+                                    <input type="text" name="product-unit-price" id="product-unit-price">
+                                </td>
+                                <td>167,00 PLN</td>
+                                <td class="green">56,00</td>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b></b></td>
+                                <td class="table-product-col">
+                                    <div class="table-product-box">
+                                        <p class="m-0">Pierwszy logotyp:</p>
+                                        <p class="m-0">L0 (grawer laserowy)</p>
+                                    </div>
+                                </td>
+                                <td>10</td>
+                                <td>
+                                    <div class="table-product-box">
+                                        <p class="m-0">Miejsca: 1</p>
+                                        <p class="m-0">Kolory: 1</p>
+                                    </div>
+                                </td>
+                                <td>0,49</td>
+                                <td>49,00</td>
+                                <td>
+                                    <input type="number" name="product-mark-up" id="product-mark-up">
+                                </td>
+                                <td>
+                                    <input type="text" name="product-unit-price" id="product-unit-price">
+                                </td>
+                                <td>167,00 PLN</td>
+                                <td class="green">39,00</td>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b></b></td>
+                                <td class="table-product-col">
+                                    <div class="table-product-box">
+                                        <p class="m-0">Drugi logotyp:</p>
+                                        <p class="m-0">L0 (grawer laserowy)</p>
+                                    </div>
+                                </td>
+                                <td>10</td>
+                                <td>
+                                    <div class="table-product-box">
+                                        <p class="m-0">Miejsca: 1</p>
+                                        <p class="m-0">Kolory: 1</p>
+                                    </div>
+                                </td>
+                                <td>0,49</td>
+                                <td>49,00</td>
+                                <td>
+                                    <input type="number" name="product-mark-up" id="product-mark-up">
+                                </td>
+                                <td>
+                                    <input type="text" name="product-unit-price" id="product-unit-price">
+                                </td>
+                                <td>167,00 PLN</td>
+                                <td class="green">39,00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="5">
+                                    <div class="bottom-offertspage-box-with-btn">
+                                        <div class="bottom-offertspage-box">
+                                            <p>Wpisz narzut dla wszystkich produktów</p>
+                                            <input class="form-input" type="number" name="all-quantity">
+                                            <button class="confirm-btn">Zatwierdź</button>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td colspan="5">
+                                    <div class="bottom-offertspage-box-with-btn">
+                                        <div class="bottom-offertspage-box">
+                                            <p>Wpisz narzut dla wszystkich pozycji znakowania</p>
+                                            <input class="form-input" type="number" name="all-quantity">
+                                            <button class="confirm-btn">Zatwierdź</button>
+                                        </div>
+                                    </div>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
-                <div class="bottom-offertspage-box-with-btn">
-                    <div class="bottom-offertspage-box">
-                        <div class="bottom-offertspage-box--text">
-                            <p>Wpisz ilość dla wszystkich pozycji:</p>
-                        </div>
-                        <div class="bottom-offertspage-box--input">
-                            <input type="number" name="all-quantity" id="all-quantity">
-                        </div>
-                        <div class="bottom-offertspage-box--btn">
-                            <button class="confirm-btn">Zatwierdź</button>
-                        </div>
+                <div class="bottom-cart-box-with-btn">
+                    <div class="bottom-cart-button">
+                        <a href="#" class="white-btn prev left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
                     </div>
-                    <div class="bottom-offertspage-button">
-                    <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                    <div class="bottom-cart-button right-btn">
+                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
                     </div>
-                </div>
-                <div class="add-product-box">
-                    <button class="add-product">Dodaj produkt</button>
                 </div>
             </div>
             <aside class="sumary_box">
@@ -127,6 +237,10 @@
                 <div class="sumary_box--brutto">
                     <p class="brutto-value">21,99 PLN</p>
                     <p class="brutto-text">Suma brutto</p>
+                </div>
+                <div class="sumary_box--profit">
+                    <p class="profit-value">21,99 PLN</p>
+                    <p class="profit-text">Twój zysk</p>
                 </div>
             </aside>
         </div>
