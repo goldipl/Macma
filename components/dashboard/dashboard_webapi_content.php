34dbc9c9647311eaeee1bfa3c5463d1aef32088d
goldipl <mgodlewski92@gmail.com>
2024-01-07 11:27:45 +0100

    add starter web api php & scss files

---
+++ components/dashboard/dashboard_webapi_content.php
@@ -0,0 +1,8 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/api.svg" alt="menu icon" width="25" height="25">Webapi</p>
+    </div>
+    <div class="dashboard-text-img-content">
+
+    </div>
+</form>
