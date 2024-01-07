735333a0c5932353dec743463f9ab14389d5d4b3
goldipl <mgodlewski92@gmail.com>
2024-01-07 11:32:48 +0100

    add links to dashboard menu

--- components/dashboard/dashboard_menu.php
+++ components/dashboard/dashboard_menu.php
@@ -8,35 +8,35 @@
     <div class="menu-links-container">
         <div class="menu-links-box big-profile">
             <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/profile.svg" alt="menu icon" width="20" height="20">Profil</a>
-            <a class="menu-link" href="#">Twoje dane</a>
-            <a class="menu-link" href="#">Newsletter</a>
-            <a class="menu-link" href="#">Program Rozwoju</a>
+            <a class="menu-link" href="./dashboard-your-data.php">Twoje dane</a>
+            <a class="menu-link" href="./dashboard-newsletter.php">Newsletter</a>
+            <a class="menu-link" href="./dashboard-program.php">Program Rozwoju</a>
         </div>
         <div class="menu-links-box big-orders">
             <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Zamówienia</a>
-            <a class="menu-link" href="#">Zamówienia</a>
-            <a class="menu-link" href="#">FV i listy przewozowe</a>
-            <a class="menu-link" href="#">Adresy dostaw</a>
-            <a class="menu-link" href="#">Wizualizacje</a>
-            <a class="menu-link" href="#">Logotypy</a>
+            <a class="menu-link" href="./dashboard-orders.php">Zamówienia</a>
+            <a class="menu-link" href="./dashboard-fv-delivery-letters.php">FV i listy przewozowe</a>
+            <a class="menu-link" href="./dashboard-delivery-addresses.php">Adresy dostaw</a>
+            <a class="menu-link" href="./dashboard-visualizations.php">Wizualizacje</a>
+            <a class="menu-link" href="./dashboard-logos.php">Logotypy</a>
         </div>
         <div class="menu-links-box big-offerts">
             <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/offerts.svg" alt="menu icon" width="20" height="20">Oferty</a>
-            <a class="menu-link" href="#">Oferty handlowe</a>
-            <a class="menu-link" href="#">Twój ofertownik</a>
-            <a class="menu-link" href="#">Kalkulacje</a>
-            <a class="menu-link" href="#">Negocjacje</a>
+            <a class="menu-link" href="./dashboard-commercial-offers.php">Oferty handlowe</a>
+            <a class="menu-link" href="./dashboard-your-offers.php">Twój ofertownik</a>
+            <a class="menu-link" href="./dashboard-calculations.php">Kalkulacje</a>
+            <a class="menu-link" href="./dashboard-negotiations.php">Negocjacje</a>
         </div>
         <div class="menu-links-box big-to-download">
             <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/to_download.svg" alt="menu icon" width="20" height="20">Do pobrania</a>
-            <a class="menu-link" href="#">Cenniki</a>
-            <a class="menu-link" href="#">Katalogi</a>
-            <a class="menu-link" href="#">Newslettery</a>
-            <a class="menu-link" href="#">Video</a>
-            <a class="menu-link" href="#">Materiały marketingowe</a>
+            <a class="menu-link" href="./dashboard-pricelists.php">Cenniki</a>
+            <a class="menu-link" href="./dashboard-catalogs.php">Katalogi</a>
+            <a class="menu-link" href="./dashboard-newsletters.php">Newslettery</a>
+            <a class="menu-link" href="./dashboard-videos.php">Video</a>
+            <a class="menu-link" href="./dashboard-marketing-materials.php">Materiały marketingowe</a>
         </div>
         <div class="menu-links-box big-web-api">
-            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/api.svg" alt="menu icon" width="20" height="20">Web API</a>
+            <a class="menu-link-big" href="./dashboard-webapi.php"><img src="./assets/icons/dashboard/api.svg" alt="menu icon" width="20" height="20">Web API</a>
         </div>
         <div class="menu-links-box big-no-name-shop">
             <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/cart.svg" alt="menu icon" width="20" height="20">Sklep No-Name</a>
