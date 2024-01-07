aec85499ea5a55970d6505516aa96c650d1e7e1c
goldipl <mgodlewski92@gmail.com>
2024-01-07 10:58:50 +0100

    fill & styling dashboard catalogs

---
+++ components/dashboard/dashboard_catalogs_content.php
@@ -0,0 +1,47 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Katalogi</p>
+    </div>
+    <div class="dashboard-catalogs">
+        <div class="dashboard-catalogs__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/catalog01.jpg" alt="catalog img" width="180" height="180">
+            </div>
+            <div class="product-content">
+                <p>Seasonal News 2023</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/eye.svg" alt="menu icon" width="24" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-catalogs__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/catalog02.jpg" alt="catalog img" width="180" height="180">
+            </div>
+            <div class="product-content">
+                <p>Sustainable</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                    <a class="delete" href="#">
+                        <img src="./assets/icons/dashboard/eye.svg" alt="menu icon" width="24" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-catalogs__slot">
+            <div class="add-catalog">
+                <div class="add-catalog-box">
+                    <img src="./assets/icons/dashboard/add_orange.svg" alt="menu icon" width="18" height="18">
+                    <span>Złóż zamówienie na katalog</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
