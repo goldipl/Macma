10a941c48dc71f38bf0dd51c1bb0d6695043f3af
goldipl <mgodlewski92@gmail.com>
2024-01-07 11:15:51 +0100

    add dashboard video page

---
+++ components/dashboard/dashboard_videos_content.php
@@ -0,0 +1,33 @@
+<form class="dashboard-wrapper">
+    <div class="title">
+        <p><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Video</p>
+    </div>
+    <div class="dashboard-img-boxes">
+        <div class="dashboard-img-boxes__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/video01.jpg" alt="newsletter img" width="220" height="220">
+            </div>
+            <div class="product-content">
+                <p>MACMA Nowości 2021</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-img-boxes__slot">
+            <div class="product-img">
+                <img src="./assets/img/dashboard/video01.jpg" alt="newsletter img" width="220" height="220">
+            </div>
+            <div class="product-content">
+                <p>MACMA Nowości 2021</p>
+                <div class="product-icons">
+                    <a class="download" href="#">
+                        <img src="./assets/icons/dashboard/download.svg" alt="menu icon" width="23" height="17">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
