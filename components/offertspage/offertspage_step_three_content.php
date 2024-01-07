a8da0efe68670e636be1a5a20b24b4fe1e8536f9
goldipl <mgodlewski92@gmail.com>
2024-01-07 17:52:39 +0100

    add offertspage step three starter files and lcean rest of pages scss

---
+++ components/offertspage/offertspage_step_three_content.php
@@ -0,0 +1,134 @@
+<div class="offertspage_container step_three">
+    <div class="container">
+        <div class="top-title-box">
+            <div class="top-title-box__left">
+                <h1>Ofertownik</h1>
+            </div>
+            <div class="top-title-box__right">
+                <a href="#" class="help-box">
+                    <img src="./assets/icons/offertspage/question-octagon.svg" alt="question icon" width="14" height="14">
+                    <span>Pomoc</span>
+                </a>
+            </div>
+        </div>
+        <div class="offertspage-table_full_box d-flex flex-row">
+            <div class="offertspage-table_box d-flex flex-column">
+                <div class="offertspage-step-table-container">
+                    <table class="offertspage-products-table">
+                        <tbody>
+                            <tr>
+                                <th>LP.</th>
+                                <th>Produkt</th>
+                                <th>Ilość</th>
+                                <th>Cena<br>katalogowa</th>
+                                <th>Rabat</th>
+                                <th>Twoja cena<br>netto szt.</th>
+                                <th>Wartość<br>netto</th>
+                                <th>Wartość<br>brutto</th>
+                                <th>Usuń</th>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b>1</b></td>
+                                <td class="table-product-col">
+                                    <div class="table-product-box">
+                                        <div class="table-product-box__top">
+                                            <p>Długopis metalowy</p>
+                                        </div>
+                                        <div class="table-product-box__bottom">
+                                            <div class="table-product-box__bottom--left">
+                                                <img src="./assets/img/offertspage/pen.jpg" alt="img" width="71" height="71">
+                                            </div>
+                                            <div class="table-product-box__bottom--right">
+                                                <p>Kod: <span>1333903</span></p>
+                                                <p>Kolor: <span>czarny</span></p>
+                                                <p>Marka: <span>Basic</span></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <input type="number" name="product-quantity" id="product-quantity">
+                                </td>
+                                <td>1,95 zł</td>
+                                <td>44%</td>
+                                <td>1,09 PLN</td>
+                                <td>1,09 PLN</td>
+                                <td>1,34 PLN</td>
+                                <td>
+                                    <a class="delete-icon" href="#">
+                                        <img src="./assets/icons/offertspage/delete.svg" alt="delete" width="17" height="20">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b>2</b></td>
+                                <td class="table-product-col">
+                                    <div class="table-product-box">
+                                        <div class="table-product-box__top">
+                                            <p>Notatnik A5</p>
+                                        </div>
+                                        <div class="table-product-box__bottom">
+                                            <div class="table-product-box__bottom--left">
+                                                <img src="./assets/img/offertspage/notepad.jpg" alt="img" width="71" height="71">
+                                            </div>
+                                            <div class="table-product-box__bottom--right">
+                                                <p>Kod: <span>1333903</span></p>
+                                                <p>Kolor: <span>czarny</span></p>
+                                                <p>Marka: <span>Basic</span></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <input type="number" name="product-quantity" id="product-quantity">
+                                </td>
+                                <td>1,95 zł</td>
+                                <td>44%</td>
+                                <td>1,09 PLN</td>
+                                <td>1,09 PLN</td>
+                                <td>1,34 PLN</td>
+                                <td>
+                                    <a class="delete-icon" href="#">
+                                        <img src="./assets/icons/offertspage/delete.svg" alt="delete" width="17" height="20">
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="bottom-offertspage-box-with-btn">
+                    <div class="bottom-offertspage-box">
+                        <div class="bottom-offertspage-box--text">
+                            <p>Wpisz ilość dla wszystkich pozycji:</p>
+                        </div>
+                        <div class="bottom-offertspage-box--input">
+                            <input type="number" name="all-quantity" id="all-quantity">
+                        </div>
+                        <div class="bottom-offertspage-box--btn">
+                            <button class="confirm-btn">Zatwierdź</button>
+                        </div>
+                    </div>
+                    <div class="bottom-offertspage-button">
+                    <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                    </div>
+                </div>
+                <div class="add-product-box">
+                    <button class="add-product">Dodaj produkt</button>
+                </div>
+            </div>
+            <aside class="sumary_box">
+                <div class="sumary_box--title">
+                    <p>Wartość produktów</p>
+                </div>
+                <div class="sumary_box--netto">
+                    <p class="netto-value">17,88 PLN</p>
+                    <p class="netto-text">Suma netto</p>
+                </div>
+                <div class="sumary_box--brutto">
+                    <p class="brutto-value">21,99 PLN</p>
+                    <p class="brutto-text">Suma brutto</p>
+                </div>
+            </aside>
+        </div>
+    </div>
+</div>
