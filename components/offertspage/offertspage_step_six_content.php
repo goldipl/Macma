e6033c69fceebf945721a21d64cc06242f4ced7a
goldipl <mgodlewski92@gmail.com>
2024-01-10 16:16:32 +0100

    fill & styling offertspage step six, last page

---
+++ components/offertspage/offertspage_step_six_content.php
@@ -0,0 +1,104 @@
+<div class="offertspage_container step_six">
+    <div class="container">
+        <div class="top-title-box">
+            <div class="top-title-box__left">
+                <h1>Ofertownik</h1>
+            </div>
+            <div class="top-title-box__right">
+                <a href="#" class="help-box">
+                    <img src="./assets/icons/offertspage/question-octagon.svg" alt="question icon" width="14" height="14">
+                    <span>Pomoc</span>
+                </a>
+            </div>
+        </div>
+        <div class="offertspage-table_full_box d-flex flex-row">
+            <div class="offertspage-table_box d-flex flex-column">
+                <div class="offertspage-step-table-container">
+                    <table class="offertspage-products-table">
+                        <tbody>
+                            <tr>
+                                <th>LP.</th>
+                                <th>Produkt</th>
+                                <th>Ilość</th>
+                                <th>Opis</th>
+                                <th>Specyfikacja</th>
+                                <th>Jednostkowa<br> cena dla klienta<br> netto/szt</th>
+                                <th>Wartość dla<br> klienta netto</th>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b>1</b></td>
+                                <td class="table-product-col">
+                                    <div class="table-product-box">
+                                        <div class="table-product-box__top">
+                                            <p>Długopis metalowy</p>
+                                        </div>
+                                        <div class="table-product-box__bottom">
+                                            <div class="table-product-box__bottom--left">
+                                                <img src="./assets/img/offertspage/pen.jpg" alt="img" width="71" height="71">
+                                            </div>
+                                            <div class="table-product-box__bottom--right">
+                                                <p>Kod: <span>1333903</span></p>
+                                                <p>Kolor: <span>czarny</span></p>
+                                                <p>Marka: <span>Basic</span></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>100</td>
+                                <td>Klasyczny długopis metalowy z chromowanymi elementami i smukłym klipsem. Długopis wyposażony w duży niebieski wkład.</td>
+                                <td class="specification">
+                                    <p>Kolor: turkusowy</p>
+                                    <p>Materiał: aluminium</p>
+                                    <p>Opakowanie: woreczek foliowy</p>
+                                    <p>Waga: 0,016</p>
+                                    <p>Rozmiar: 13,67 x 1cm</p>
+                                </td>
+                                <td>1,67 PLN</td>
+                                <td>167,09 PLN</td>
+                            </tr>
+                            <tr>
+                                <td class="table-number-col"><b>2</b></td>
+                                <td>Pierwszy logotyp</td>
+                                <td>100</td>
+                                <td class="specification">L0 (grawer laserowy)</td>
+                                <td>Miejsca: 1, kolory: 1</td>
+                                <td>0,88 PLN</td>
+                                <td>88,00 PLN</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="bottom-offertspage-box-with-btn">
+                    <div class="bottom-offertspage-button">
+                        <a href="#" class="white-btn prev left-btn">
+                            <img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót </a>
+                    </div>
+                    <div class="add-product-box">
+                        <button class="add-product">Dodaj produkt</button>
+                        <button class="add-product">Przekształć w zamówienie <img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></button>
+                        <button class="add-product">Pobierz plik PDF <img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></button>
+                        <button class="add-product">Drukuj <img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></button>
+                        <button class="add-product">Wyślij <img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></button>
+                    </div>
+                    <div class="bottom-offertspage-button right-btn">
+                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <aside class="sumary_box">
+                <div class="sumary_box--title">
+                    <p>Wartość produktów</p>
+                </div>
+                <div class="sumary_box--netto">
+                    <p class="netto-value">17,88 PLN</p>
+                    <p class="netto-text">Suma netto</p>
+                </div>
+                <div class="sumary_box--brutto">
+                    <p class="brutto-value">21,99 PLN</p>
+                    <p class="brutto-text">Suma brutto</p>
+                </div>
+            </aside>
+        </div>
+    </div>
+</div>
