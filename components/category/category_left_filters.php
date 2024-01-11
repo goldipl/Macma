7a5d0b5a63bce162da7aa560a0ba458cebdedc1d
goldipl <mgodlewski92@gmail.com>
2024-01-11 17:56:30 +0100

    fill & styling category menu

--- components/category/category_left_filters.php
+++ components/category/category_left_filters.php
@@ -1,3 +1,102 @@
 <div class="category-left-filters">
-    
+    <div class="category-left-filters__slot">
+        <div class="header">
+            <h3>Sortuj</h3>
+        </div>
+        <div class="content">
+            <label class="custom-checkbox-container">Nowość
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container">Bestseller
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container">Promocja
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container">Ekologiczny
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+    </div>
+    <div class="category-left-filters__slot">
+        <div class="header">
+            <h3>Typ ceny</h3>
+        </div>
+        <div class="content">
+            <label class="custom-checkbox-container">Katalogowa
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container">Twoja cena
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+    </div>
+    <div class="category-left-filters__slot">
+        <div class="header">
+            <h3>Cena</h3>
+        </div>
+        <div class="content">
+            <div class="row-inputs">
+                <div class="row-inputs__left">
+                    <input class="form-input" type="number" name="price-left-input" id="price-left-input" placeholder="Od">
+                </div>
+                <span class="spacer">-</span>
+                <div class="row-inputs__right">
+                <input class="form-input" type="number" name="price-right-input" id="price-right-input" placeholder="Do">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="category-left-filters__slot">
+        <div class="header">
+            <h3>Ilość</h3>
+        </div>
+        <div class="content">
+            <input class="form-input" type="number" name="quantity-input" id="quantity-input">
+        </div>
+    </div>
+    <div class="category-left-filters__slot">
+        <div class="header">
+            <h3>Dostępność</h3>
+        </div>
+        <div class="content">
+            <label class="custom-checkbox-container">24h/48h
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container">2-3 dni
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container">Dostawa
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+    </div>
+    <div class="category-left-filters__slot">
+        <div class="header">
+            <h3>Kolor</h3>
+        </div>
+        <div class="content">
+            <label class="custom-checkbox-container"><div class="color white"></div>Biały
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container"><div class="color black"></div>Czarny
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+            <label class="custom-checkbox-container"><div class="color red"></div>Czerwony
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+    </div>
 </div>
