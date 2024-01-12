814a91885936fb074d95b612d495707463847a9a
goldipl <mgodlewski92@gmail.com>
2024-01-12 12:46:06 +0100

    styling & add category top filters

--- components/category/category_top_filters.php
+++ components/category/category_top_filters.php
@@ -1,3 +1,23 @@
 <div class="category-top-filters">
-    
+    <div class="category-top-filters__slot">
+        <label for="show-all-colors">Pokaż wszystkie kolory:</label>
+        <select name="show-all-colors" id="show-all-colors" class="form-select">
+            <option value="Nie">Nie</option>
+            <option value="Tak">Tak</option>
+        </select>
+    </div>
+    <div class="category-top-filters__slot">
+        <label for="sort-by">Sortuj wg:</label>
+        <select name="sort-by" id="sort-by" class="form-select">
+            <option value="Nazwa produktu A-Z">Nazwa produktu A-Z</option>
+            <option value="Nazwa produktu Z-A">Nazwa produktu Z-A</option>
+        </select>
+    </div>
+    <div class="category-top-filters__slot">
+        <label for="show-quantity">Wyświetl:</label>
+        <select name="show-quantity" id="show-quantity" class="form-select">
+            <option value="16">16</option>
+            <option value="32">32</option>
+        </select>
+    </div>
 </div>
