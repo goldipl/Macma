c34ad72c65927bdd3be31ef3693ebb8a183a488f
goldipl <mgodlewski92@gmail.com>
2024-01-14 11:01:38 +0100

    add register page & starter scss files

---
+++ components/register/hero.php
@@ -0,0 +1,27 @@
+<section id="hero" class="hero container register-purple">
+    <div class="hero_content">
+        <div class="breadcrumbs container">
+            <ul>
+                <li class="breadcrumb-item">
+                    <a href="./index.php">Home</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="#">Rejestracja</a>
+                </li>
+            </ul>
+        </div>
+        <h1 class="hero_content--title">Tekst</h1>
+        <p class="hero_content--text">W odpowiedzi na potrzeby naszych Klientów wprowadziliśmy do oferty usługę Direct Asia, w ramach której realizowana jest kompleksowa obsługa zamówień indywidualnych bezpośrednio u dostawców na Dalekim Wschodzie.</p>
+        <div class="hero-btns-box">
+            <div class="hero_content--btn">
+                <a href="#">Jestem nowym klientem <br>Macma Polska<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+            </div>
+            <div class="hero_content--btn">
+                <a href="#">Jestem już klientem <br>Macma Polska <br>i zakładam dodatkowe konto<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+            </div>
+        </div>
+    </div>
+    <div class="hero_image">
+        <img src="./assets/img/register/hero_img.jpg" alt="hero img" width="1928" height="638">
+    </div>
+</section>
---
+++ components/register/register_pl_content.php
@@ -0,0 +1,8 @@
+<div class="grey-background">
+    <div class="register container">
+        <h1>Jestem nowym klientem <span>Macma Polska</span></h1>
+        <form class="register-form">
+            
+        </form>
+    </div>
+</div>
