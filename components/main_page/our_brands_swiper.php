8cc6a110d684f8bcafc5570249b9a716ca40aff4
goldipl <mgodlewski92@gmail.com>
2024-01-15 10:11:43 +0100

    add more logos

--- components/main_page/our_brands_swiper.php
+++ components/main_page/our_brands_swiper.php
@@ -38,6 +38,42 @@
                     <img src="./assets/img/main_page/our_brands/pierre_cardin.jpg" alt="brand img" width="222" height="111">
                 </a>
             </div>
+            <!-- Slide 01 -->
+            <div class="swiper-slide">
+                <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
+                    <img src="./assets/img/main_page/our_brands/Schwarzwolf.jpg" alt="brand img" width="222" height="111">
+                </a>
+            </div>
+            <!-- Slide 02 -->
+            <div class="swiper-slide">
+                <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
+                    <img src="./assets/img/main_page/our_brands/M-collections.jpg" alt="brand img" width="222" height="111">
+                </a>
+            </div>
+            <!-- Slide 03 -->
+            <div class="swiper-slide">
+                <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
+                    <img src="./assets/img/main_page/our_brands/ferraghini.jpg" alt="brand img" width="222" height="111">
+                </a>
+            </div>
+             <!-- Slide 04 -->
+             <div class="swiper-slide">
+                <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
+                    <img src="./assets/img/main_page/our_brands/crisma.jpg" alt="brand img" width="222" height="111">
+                </a>
+            </div>
+            <!-- Slide 05 -->
+            <div class="swiper-slide">
+                <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
+                    <img src="./assets/img/main_page/our_brands/mark_twain.jpg" alt="brand img" width="222" height="111">
+                </a>
+            </div>
+            <!-- Slide 06 -->
+            <div class="swiper-slide">
+                <a href="#" class="d-flex flex-column align-items-center justify-content-center text-decoration-none">
+                    <img src="./assets/img/main_page/our_brands/pierre_cardin.jpg" alt="brand img" width="222" height="111">
+                </a>
+            </div>
         </div>
         <!-- If we need navigation buttons -->
         <div class="swiper-button-prev"></div>
