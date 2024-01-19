38502acefdb14edd62176ab30de10de989a2c17b
goldipl <mgodlewski92@gmail.com>
2024-01-19 16:01:38 +0100

    change hero swiper add clickable button

--- components/main_page/hero_swiper.php
+++ components/main_page/hero_swiper.php
@@ -1,4 +1,4 @@
-<section id="swiper" class="container">
+<section id="swiper" class="container main-page-swiper">
     <!-- Slider main container -->
     <div class="swiper desktop">
         <!-- Additional required wrapper -->
@@ -8,21 +8,41 @@
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper.jpg" alt="banner" width="1200" height="383">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                    <p>Zobacz co zamawiają inni</p>
+                    <a class="white-btn" href="#our_products_container">Kliknij i sprawdź<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                </div>
             </div>
             <div class="swiper-slide">
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper.jpg" alt="banner" width="1200" height="383">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                    <p>Zobacz co zamawiają inni</p>
+                    <a class="white-btn" href="#our_products_container">Kliknij i sprawdź<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                </div>
             </div>
             <div class="swiper-slide">
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper.jpg" alt="banner" width="1200" height="383">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                    <p>Zobacz co zamawiają inni</p>
+                    <a class="white-btn" href="#our_products_container">Kliknij i sprawdź<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                </div>
             </div>
             <div class="swiper-slide">
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper.jpg" alt="banner" width="1200" height="383">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                    <p>Zobacz co zamawiają inni</p>
+                    <a class="white-btn" href="#our_products_container">Kliknij i sprawdź<img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"></a>
+                </div>
             </div>
         </div>
         <!-- If we need pagination -->
@@ -39,21 +59,33 @@
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile.jpg" alt="banner" width="491" height="405">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                </div>
             </div>
             <div class="swiper-slide">
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile.jpg" alt="banner" width="491" height="405">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                </div>
             </div>
             <div class="swiper-slide">
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile.jpg" alt="banner" width="491" height="405">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                </div>
             </div>
             <div class="swiper-slide">
                 <a href="#our_products_container">
                     <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile.jpg" alt="banner" width="491" height="405">
                 </a>
+                <div class="hero_content--content">
+                    <h1><span>TOP 10</span> gadżetów<br>reklamowych</h1>
+                </div>
             </div>
         </div>
         <!-- If we need pagination -->
