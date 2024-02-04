846647425613c3c0a68b468c18f32500736c0f27
goldipl <mgodlewski92@gmail.com>
2024-02-04 12:29:33 +0100

    change contact hours content & padding

--- components/contact/contact_other_departments_content.php
+++ components/contact/contact_other_departments_content.php
@@ -7,25 +7,25 @@
             <b class="graphic-department">Dział Graficzny</b>
             <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
             <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
         </div>
         <div class="other-department-box__row__col">
             <b class="accountancy">Księgowość</b>
             <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
             <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
         </div>
         <div class="other-department-box__row__col">
             <b class="marketing-department">Dział Marketingu</b>
             <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
             <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
         </div>
         <div class="other-department-box__row__col">
             <b class="import-department">Dział Importu</b>
             <p class="phone"><a class="gtm-phone" href="tel:+48 71 330 54 60">+48 71 330 54 60</a></p>
             <p class="mail"><a class="gtm-mail" href="mailto:biuro@macma.pl">biuro@macma.pl</a></p>
-            <p class="hours">Biuro czynne: pon-pią 8:00-16:00</p>
+            <p class="hours">Biuro czynne: pon. - pt. 8:00 – 16:00</p>
         </div>
     </div>
 </div>
