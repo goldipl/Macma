2ec2188a4a1003b023a1f8315c99e51025c75853
goldipl <mgodlewski92@gmail.com>
2024-02-08 15:34:14 +0100

    correct cart step three scroll section

--- components/cart/cart_step_three_content_delivery.php
+++ components/cart/cart_step_three_content_delivery.php
@@ -1,7 +1,7 @@
 <div class="cart_container step_three">
     <div class="container">
         <h1>Wysyłka</h1>
-        <div class="cart-table_full_box d-flex flex-row">
+        <div class="cart-table_full_box d-flex flex-column">
             <div class="cart-table_box d-flex flex-column">
                 <div class="cart-step-table-container">
                     <table class="cart-products-table">
@@ -170,13 +170,13 @@
                         </tbody>
                     </table>
                 </div>
-                <div class="bottom-cart-box-with-btn">
-                    <div class="bottom-cart-button">
-                        <a href="#" class="white-btn prev left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
-                    </div>
-                    <div class="bottom-cart-button right-btn">
-                        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                    </div>
+            </div>
+            <div class="bottom-cart-box-with-btn">
+                <div class="bottom-cart-button">
+                    <a href="#" class="white-btn prev left-btn"><img src="./assets/icons/common/buttons/button_orange_arrow.svg" alt="arrow icon" width="18" height="18"> Powrót</a>
+                </div>
+                <div class="bottom-cart-button right-btn">
+                    <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
                 </div>
             </div>
         </div>
