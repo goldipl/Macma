840ca637105aa584190d27a916db25090d63d92d
goldipl <mgodlewski92@gmail.com>
2024-03-24 14:00:27 +0100

    add target="_blank" & rel="nofollow" to socials links

--- components/common/topbar.php
+++ components/common/topbar.php
@@ -1,16 +1,16 @@
 <div id="topbar">
     <div class="container d-flex justify-content-between align-items-center">
         <div class="d-flex flex-row">
-            <a href="#" class="p-1">
+            <a href="https://www.facebook.com/MacmaPolska/" class="p-1" rel="nofollow" target="_blank">
                 <img src="./assets/icons/common/topbar/facebook.svg" alt="facebook" width="26" height="26">
             </a>
-            <a href="#" class="p-1">
+            <a href="https://www.youtube.com/channel/UCc1vJYla9NRpnTaPV5hXjSA" class="p-1" rel="nofollow" target="_blank">
                 <img src="./assets/icons/common/topbar/youtube.svg" alt="youtube" width="26" height="26">
             </a>
-            <a href="#" class="p-1">
+            <a href="https://www.instagram.com/macma_polska/" class="p-1" rel="nofollow" target="_blank">
                 <img src="./assets/icons/common/topbar/instagram.svg" alt="instagram" width="26" height="26">
             </a>
-            <a href="#" class="p-1">
+            <a href="https://www.linkedin.com/company/macma-polska/" class="p-1" rel="nofollow" target="_blank">
                 <img src="./assets/icons/common/topbar/linkedin.svg" alt="linkedin" width="26" height="26">
             </a>
         </div>
