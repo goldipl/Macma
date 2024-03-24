6aad6f9b7723d1eef769e80b524bad1e6f386820
goldipl <mgodlewski92@gmail.com>
2024-03-24 16:20:12 +0100

    correct titles tags, add additional <h3>

--- components/news_article/news_article_more_news.php
+++ components/news_article/news_article_more_news.php
@@ -25,7 +25,9 @@
             <p>Poniedziałek, 12 czerwca 2023</p>
         </div>
         <div class="news_articles__slot--title">
-            <a href="#">Ulepszamy naszą drukarnię!</a>
+            <h3>
+                <a href="#">Ulepszamy naszą drukarnię!</a>
+            </h3>
         </div>
         <div class="news_articles__slot--text">
             <p>Nadruki i gadżety reklamowe to niesamowicie skuteczne połączenie. Żeby zapewnić Wam jak najwyższą jakość i jak najlepszy serwis podjęliśmy decyzję o rozbudowie naszej drukarni</p>
@@ -42,7 +44,9 @@
             <p>Poniedziałek, 12 czerwca 2023</p>
         </div>
         <div class="news_articles__slot--title">
-            <a href="#">Złota noc</a>
+            <h3>
+                <a href="#">Złota noc</a>
+            </h3>
         </div>
         <div class="news_articles__slot--text">
             <p>Jako hurtownia gadżetów reklamowych działamy nieprzerwanie już od 20 lat! Niedawno mieliśmy okazją śwętować ten okrągły jubileusz.</p>
@@ -59,7 +63,9 @@
             <p>Poniedziałek, 12 czerwca 2023</p>
         </div>
         <div class="news_articles__slot--title">
-            <a href="#">Ekipa do zadań specjalnych</a>
+            <h3>
+                <a href="#">Ekipa do zadań specjalnych</a>
+            </h3>
         </div>
         <div class="news_articles__slot--text">
             <p>Poznajcie pierwszoplanowe gwiazdy, które wcielą się w każdą rolę, żeby zareklamować biznes Waszych Klientów!</p>
@@ -76,7 +82,9 @@
             <p>Poniedziałek, 12 czerwca 2023</p>
         </div>
         <div class="news_articles__slot--title">
-            <a href="#">PROMO SHOW 8-9 lutego 2023 - czekamy na Was!</a>
+            <h3>
+                <a href="#">PROMO SHOW 8-9 lutego 2023 - czekamy na Was!</a>
+            </h3>
         </div>
         <div class="news_articles__slot--text">
             <p>Zapraszamy wszystkich na nasze stoisko, a tych którzy nie mogą być z nami prosimy o cierpliwość.</p>
