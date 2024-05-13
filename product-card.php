a74b7bbf512afcc72d65d99b8aa6440618b98974
goldipl <mgodlewski92@gmail.com>
2024-05-13 20:08:39 +0200

    handle download products img

--- product-card.php
+++ product-card.php
@@ -51,6 +51,22 @@
         <script src="./js/jquery.magnific-popup.min.js"></script>
         <script>
             $(document).ready(function() {
+                 // Add click event listener to product-download icons
+                 $(".product-download").click(function(e){
+                    e.preventDefault(); // Prevent default action of anchor tag
+
+                    // Get the URL of the image to be downloaded
+                    var imageUrl = $(this).closest(".swiper-slide").find("img").attr("src");
+
+                    // Create a temporary link element to trigger download
+                    var downloadLink = document.createElement("a");
+                    downloadLink.href = imageUrl;
+                    downloadLink.download = "product_image.jpg"; // You can set the filename here
+                    downloadLink.style.display = "none";
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                    document.body.removeChild(downloadLink);
+                });
                 // Initialize Magnific Popup for .zoom-image elements
                 $('.zoom-image').magnificPopup({
                     type: 'image',
