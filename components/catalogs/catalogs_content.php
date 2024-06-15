20eaaeedfdb18cf455d1b09f64e31404921a7d7e
goldipl <mgodlewski92@gmail.com>
2024-06-15 11:37:47 +0200

    add a new catalog to template

--- components/catalogs/catalogs_content.php
+++ components/catalogs/catalogs_content.php
@@ -5,11 +5,11 @@
         <div class="catalogs-wrapper_col">
             <div class="catalogs-wrapper_col_top">
                 <div class="catalogs-wrapper_col_top_left">
-                    <img src="./assets/img/catalogs/catalogs-01.jpg" alt="Katalog" width="248" height="349">
+                    <img src="./assets/img/catalogs/catalogs-00.jpg" alt="Katalog" width="248" height="349">
                 </div>
                 <div class="catalogs-wrapper_col_top_right">
                     <div class="catalogs-text">
-                        <h2>Seasonal news 2023</h2>
+                        <h2>Macma News 2024</h2>
                         <p>Lorem ipsum dolor sit amet consectetur. Magna eget nunc tincidunt gravida eu scelerisque duis tincidunt. Lacinia sed a id non lorem. Urna ultricies nec sed sed volutpat. Enim a maecenas nulla sit ipsum sociis lorem aliquam. Consectetur nam mauris neque ipsum ultrices odio quis amet integer. Nunc faucibus molestie quis ac in. Urna et senectus amet velit in. Ut urna urna risus tellus vitae senectus facilisi vel. Id risus vitae lorem mi felis pretium ut ultricies. Facilisis dictum orci egestas at. Vitae integer diam ultricies leo rutrum urna. Vulputate etiam id tincidunt massa elit nulla. Commodo dignissim suspendisse etiam convallis parturient eget sed enim. Augue sem enim aliquam in justo venenatis gravida. Vitae ipsum quam feugiat integer vitae nulla quis sed tempor. Quis dictum et nunc ut enim donec. Mattis dolor sed dignissim quam feugiat erat sagittis.</p>
                     </div>
                 </div>
@@ -39,6 +39,35 @@
                 <div class="catalogs-wrapper_col_top_right">
                     <div class="catalogs-text">
                         <h2>SUSTAINABLE PROMOTION PRODUCTS</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur. Magna eget nunc tincidunt gravida eu scelerisque duis tincidunt. Lacinia sed a id non lorem. Urna ultricies nec sed sed volutpat. Enim a maecenas nulla sit ipsum sociis lorem aliquam. Consectetur nam mauris neque ipsum ultrices odio quis amet integer. Nunc faucibus molestie quis ac in. Urna et senectus amet velit in. Ut urna urna risus tellus vitae senectus facilisi vel. Id risus vitae lorem mi felis pretium ut ultricies. Facilisis dictum orci egestas at. Vitae integer diam ultricies leo rutrum urna. Vulputate etiam id tincidunt massa elit nulla. Commodo dignissim suspendisse etiam convallis parturient eget sed enim. Augue sem enim aliquam in justo venenatis gravida. Vitae ipsum quam feugiat integer vitae nulla quis sed tempor. Quis dictum et nunc ut enim donec. Mattis dolor sed dignissim quam feugiat erat sagittis.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="catalogs-wrapper_col_bottom">
+                <div class="catalogs-wrapper_col_bottom__btns">
+                    <a href="" class="catalogs-wrapper_col_bottom__btns--btn">
+                        <span>ZOBACZ ONLINE</span>
+                        <img src="./assets/icons/catalogs/eye.svg" alt="eye" width="24" height="20">
+                    </a>
+                    <a href="" class="catalogs-wrapper_col_bottom__btns--btn">
+                        <span>POBIERZ</span>
+                        <img src="./assets/icons/catalogs/download.svg" alt="download" width="20" height="20">
+                    </a>
+                    <a href="" class="catalogs-wrapper_col_bottom__btns--btn">
+                        <span>ZAMÓW KATALOG DRUKOWANY</span>
+                        <img src="./assets/icons/catalogs/cart.svg" alt="cart" width="20" height="20">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="catalogs-wrapper_col">
+            <div class="catalogs-wrapper_col_top">
+                <div class="catalogs-wrapper_col_top_left">
+                    <img src="./assets/img/catalogs/catalogs-01.jpg" alt="Katalog" width="248" height="349">
+                </div>
+                <div class="catalogs-wrapper_col_top_right">
+                    <div class="catalogs-text">
+                        <h2>Seasonal news 2023</h2>
                         <p>Lorem ipsum dolor sit amet consectetur. Magna eget nunc tincidunt gravida eu scelerisque duis tincidunt. Lacinia sed a id non lorem. Urna ultricies nec sed sed volutpat. Enim a maecenas nulla sit ipsum sociis lorem aliquam. Consectetur nam mauris neque ipsum ultrices odio quis amet integer. Nunc faucibus molestie quis ac in. Urna et senectus amet velit in. Ut urna urna risus tellus vitae senectus facilisi vel. Id risus vitae lorem mi felis pretium ut ultricies. Facilisis dictum orci egestas at. Vitae integer diam ultricies leo rutrum urna. Vulputate etiam id tincidunt massa elit nulla. Commodo dignissim suspendisse etiam convallis parturient eget sed enim. Augue sem enim aliquam in justo venenatis gravida. Vitae ipsum quam feugiat integer vitae nulla quis sed tempor. Quis dictum et nunc ut enim donec. Mattis dolor sed dignissim quam feugiat erat sagittis.</p>
                     </div>
                 </div>
