173dfd621fa6d18063e62ce84c5127af7674cc89
goldipl <mgodlewski92@gmail.com>
2024-10-07 18:33:21 +0200

    add e-leaflet subpage and styles for new subpage

---
+++ components/e_leaflet/e_leaflet_wrapper.php
@@ -0,0 +1,203 @@
+<div class="product_card_gallery_info container">
+    <div class="product-gallery">
+        <div class="container">
+            <!-- top gallery -->
+            <div class="swiper gallery article-gallery slider-one">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="swiper-zoom-container">
+                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <!-- Product icons -->
+                        <div class="product-icons">
+                            <span class="product-zoom-in">
+                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+                            </span>
+                            <a class="product-download" href="#">
+                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-zoom-container">
+                            <img class="zoom-image" src="./assets/img/product_card/product-card-02.jpg" href="./assets/img/product_card/product-card-02.jpg">
+                        </div>
+                        <!-- Product icons -->
+                        <div class="product-icons">
+                            <span class="product-zoom-in">
+                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+                            </span>
+                            <a class="product-download" href="#">
+                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-zoom-container">
+                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <!-- Product icons -->
+                        <div class="product-icons">
+                            <span class="product-zoom-in">
+                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+                            </span>
+                            <a class="product-download" href="#">
+                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-zoom-container">
+                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <!-- Product icons -->
+                        <div class="product-icons">
+                            <span class="product-zoom-in">
+                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+                            </span>
+                            <a class="product-download" href="#">
+                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-zoom-container">
+                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <!-- Product icons -->
+                        <div class="product-icons">
+                            <span class="product-zoom-in">
+                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+                            </span>
+                            <a class="product-download" href="#">
+                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Product label -->
+                <div class="product-label-box">
+                    <div class="product-label" style="background-color: #E4EDFF;">
+                        <p>Promocja</p>
+                    </div>
+                </div>
+            </div>
+            <!-- thumbs -->
+            <div class="thumbs-overflow-wrapper">
+                <div class="swiper gallery-thumbs slider-two">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/img/product_card/product-card-02.jpg">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/img/product_card/product-card-01.jpg">
+                        </div>
+                    </div>
+                </div>
+                <!-- Add Arrows -->
+                <div title="next" class="swiper-button-next"></div>
+                <div title="prev" class="swiper-button-prev"></div>
+            </div>
+        </div>
+    </div>
+    <div class="e-leaflet-info">
+        <div class="e-leaflet-info_title">
+            <h1>Stwórz E-ULOTKĘ</h1>
+        </div>
+        <label for="discount">Nazwa:</label>
+        <input class="form-input w-100" type="text" name="e-leaflet-name" id="e-leaflet-name">
+        <label for="discount">Opis:</label>
+        <textarea class="form-textarea w-100" type="text" name="e-leaflet-desc" id="e-leaflet-desc"></textarea>
+        <div class="d-flex flex-row flex-wrap gap-3 row-inputs">
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-code">Kod:</label>
+                <input class="form-input" type="text" name="e-leaflet-code" id="e-leaflet-code">
+            </div>
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-ean">EAN:</label>
+                <input class="form-input" type="text" name="e-leaflet-ean" id="e-leaflet-ean">
+            </div>
+        </div>
+        <div class="d-flex flex-row flex-wrap gap-3 row-inputs">
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-color">Kolor:</label>
+                <input class="form-input" type="text" name="e-leaflet-color" id="e-leaflet-color">
+            </div>
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-nettoprice">Cena netto:</label>
+                <input class="form-input" type="text" name="e-leaflet-nettoprice" id="e-leaflet-nettoprice">
+            </div>
+        </div>
+        <h3 class="e-leaflet-info_subtitle mt-3">Dane Firmowe:</h3>
+        <div class="d-flex flex-row flex-wrap gap-3 row-inputs">
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-company">Firma:</label>
+                <input class="form-input" type="text" name="e-leaflet-company" id="e-leaflet-company">
+            </div>
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-city">Miasto:</label>
+                <input class="form-input" type="text" name="e-leaflet-city" id="e-leaflet-city">
+            </div>
+        </div>
+        <div class="d-flex flex-row flex-wrap gap-3 row-inputs">
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-street">Ulica:</label>
+                <input class="form-input" type="text" name="e-leaflet-street" id="e-leaflet-street">
+            </div>
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-telephone">Tel.:</label>
+                <input class="form-input" type="text" name="e-leaflet-telephone" id="e-leaflet-telephone">
+            </div>
+        </div>
+        <div class="d-flex flex-row flex-wrap gap-3 row-inputs">
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-zipcode">Kod pocztowy:</label>
+                <input class="form-input" type="text" name="e-leaflet-zipcode" id="e-leaflet-zipcode">
+            </div>
+            <div class="d-flex flex-column column-inputs">
+                <label for="e-leaflet-nipcode">NIP:</label>
+                <input class="form-input" type="text" name="e-leaflet-nipcode" id="e-leaflet-nipcode">
+            </div>
+        </div>
+        <label for="e-leaflet-comments">Uwagi:</label>
+        <textarea class="form-textarea w-100" type="text" name="e-leaflet-comments" id="e-leaflet-comments"></textarea>
+        <h3 class="e-leaflet-info_subtitle subtitle-with-underline mt-3">Zaznacz elementy, które powinny znaleźć się na e-ulotce:</h3>
+        
+        <div class="inline-flex flex-wrap">
+            <div class="form-check form-switch">
+                <label class="form-check-label" for="flexSwitchZnakowanie">Znakowanie:</label>
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchZnakowanie">
+            </div>
+            <div class="form-check form-switch">
+                <label class="form-check-label" for="flexSwitchSpecyfikacja">Specyfikacja:</label>
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchSpecyfikacja">
+            </div>
+            <div class="form-check form-switch">
+                <label class="form-check-label" for="flexSwitchOpakowanie">Opakowanie:</label>
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchOpakowanie">
+            </div>
+            <div class="form-check form-switch">
+                <label class="form-check-label" for="flexSwitchPodobneProdukty">Podobne produkty:</label>
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchPodobneProdukty">
+            </div>
+        </div>
+        
+        <div class="e-leaflet__btns">
+            <a href="" class="e-leaflet__btns--btn">
+                <span>Pobierz ulotkę PDF</span>
+                <img src="./assets/icons/catalogs/download.svg" alt="download" width="20" height="20">
+            </a>
+            <button class="orange-btn">Wyślij e-mailem do klienta <img src="./assets/icons/common/buttons/button_white_arrow.svg"></button>
+        </div>
+    </div>
+</div>
+
---
+++ e-leaflet.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MACMA - E-Ulotka</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/swiper-bundle.css" />
+        <link rel="stylesheet" href="./css/select2.min.css" />
+        <link rel="stylesheet" href="./css/magnific-popup.min.css">
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body>
+        <header> 
+            <?php include "./components/common/topbar.php"; ?> 
+            <?php include "./components/common/nav.php"; ?> 
+        </header>
+        <main id="main-wrapper">
+            <div class="breadcrumbs container">
+                <ul>
+                    <li class="breadcrumb-item">
+                        <a href="./index.php">Home</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="#">Generator ulotek PDF</a>
+                    </li>
+                </ul>
+            </div>
+            <?php include "./components/e_leaflet/e_leaflet_wrapper.php"; ?>  
+        </main>
+        <footer> 
+            <?php include "./components/common/footer.php"; ?> 
+        </footer>
+        <script src="./js/jquery.min.js"></script>
+        <script src="./js/popper.min.js"></script>
+        <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="./js/bootbox.min.js"></script>
+        <script src="./js/select2.min.js"></script>
+        <script src="./js/script.js"></script>
+        <script src="./js/swiper/swiper-bundle.js"></script>
+        <script src="./js/products_card_gallery/products_card_gallery.js"></script>
+        <script src="./js/jquery.magnific-popup.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                 $(".product-download").click(function(e){
+                    e.preventDefault(); 
+
+                    var imageUrl = $(this).closest(".swiper-slide").find("img").attr("src");
+
+                    var downloadLink = document.createElement("a");
+                    downloadLink.href = imageUrl;
+                    downloadLink.download = "product_image.jpg";
+                    downloadLink.style.display = "none";
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                    document.body.removeChild(downloadLink);
+                });
+                
+                $('.zoom-image').magnificPopup({
+                    type: 'image',
+                    gallery: {
+                        enabled: true
+                    },
+                });
+
+                $('.zoom-icon').on('click', function() {
+                    $(this).closest('.swiper-slide').find('.zoom-image').magnificPopup('open');
+                });
+            });
+        </script>
+    </body>
+</html>
