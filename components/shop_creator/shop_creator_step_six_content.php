d28af980d7ef0feb6e2ff1d1724c64e5e0f32258
goldipl <mgodlewski92@gmail.com>
2024-11-09 14:57:51 +0100

    add all shop creator steps starter templates

---
+++ components/shop_creator/shop_creator_step_six_content.php
@@ -0,0 +1,19 @@
+<div class="shopcreator-content container">
+    <h3>Text</h3>
+    <div class="textbox">
+        <p>Text</p>
+    </div>
+    <div class="bottom-button right-btn">
+        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+        </a>
+    </div>
+    <div class="shopcreator-dots">
+        <div class="shopcreator-dots__dot"></div>
+        <div class="shopcreator-dots__dot"></div>
+        <div class="shopcreator-dots__dot"></div>
+        <div class="shopcreator-dots__dot"></div>
+        <div class="shopcreator-dots__dot"></div>
+        <div class="shopcreator-dots__dot active"></div>
+        <div class="shopcreator-dots__dot"></div>
+    </div>
+</div>
