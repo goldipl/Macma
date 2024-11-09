abdaf99aa821b82740aa95ac0d85549966a2a0e4
goldipl <mgodlewski92@gmail.com>
2024-11-09 16:14:25 +0100

    fill and style step three shop creator

--- components/shop_creator/shop_creator_step_three_content.php
+++ components/shop_creator/shop_creator_step_three_content.php
@@ -1,11 +1,149 @@
-<div class="shopcreator-content container">
-    <h3>Text</h3>
+<form class="shopcreator-content container">
+    <h3>Wprowadź dane teleadresowe Twojej firmy.</h3>
     <div class="textbox">
-        <p>Text</p>
+        <p>Pojawią się one jako wizytówka firmy w stopce strony.</p>
+    </div>
+    <div class="shopcreator-labels d-flex flex-row flex-wrap gap-3">
+        <div class="d-flex flex-column label-box">
+            <label for="shop-name">Nazwa sklepu/katalogu lub firmy</label>
+            <input class="form-input" type="text" name="shop-name" id="shop-name">
+        </div>
+    </div>
+    <div class="shopcreator-labels d-flex flex-row flex-wrap gap-3">
+        <div class="d-flex flex-column label-box">
+            <label for="shop-desc">Opis Firmy:</label>
+            <textarea class="form-textarea" name="shop-desc" id="shop-desc"></textarea>
+            <p>Opis firmy lub sklepu. Będzie widoczny w sklepie no-name w zakładce “Kontakt” oraz jako opis strony podczasudostępniania jej np. w mediach społecznościowych.</p>
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-email">Adres email:</label>
+            <input class="form-input" type="email" name="shop-email" id="shop-email">
+            <p>Wpisz swój adres e-mail do kontaktu dla klientów. Będziesz otrzymywać na tą skrzynkę zapytania ofertowe od klientów.</p>
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-contactdata">Dane kontaktowe:</label>
+            <textarea class="form-textarea" name="shop-contactdata" id="shop-contactdata"></textarea>
+            <p>Dane teleadresowe firmy. Wpisane dane będą widoczne na stronie głównej oraz w stopce w mailach automatycznych,które będą otrzymywać Twoi klienci po wysłaniu zapytania lub złożeniu zamówienia (tylko dla sklepów internetowych).</p>
+        </div>
+        <div class="d-flex flex-column label-box">
+            <span class="logo-label">Logo:</span>
+            <label for="files" class="upload-files-box">
+                <img src="./assets/icons/upload-icon.svg" alt="upload icon" width="21" height="14">
+                <p>Przeciągnij i upuść albo <br>
+                    <span>Dodaj plik</span>
+                </p>
+                <input class="file file-block" type="file" id="files02">
+            </label>
+            <p>Dane teleadresowe firmy Wpisane dane będą widoczne na stronie głównej oraz w stopce w mailach automatycznych, które będą otrzymywać Twoi klienci po wysłaniu zapytania lub złożeniu zamówienia (tylko dla sklepów internetowych).</p>
+        </div>
+    </div>
+    <strong>Szablon kolorystyczny:</strong>
+    <p class="mt-2">Wybierz szablon kolorystyczny Twojej strony. Dzięki niemu Twoja strona będzie spójna wizualnie.</p>
+    <div class="shopcreator-checks d-flex flex-row flex-wrap gap-3">
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>beżowy</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>niebieski</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>czarny</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>pomarańczowy</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>czerwony</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>różowy</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>fioletowy</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>szary</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>granatowy</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-checks__slot short-slot">
+            <label class="custom-checkbox-container">
+                <strong>zielony</strong>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+    </div>
+    <strong>Media społecznościowe:</strong>
+    <p class="mt-2">Wpisz adresy kierujące do Twoich profili w mediach społecznościowych, wówczas na stronie głównej pojawią się ikony
+wybranych portali wraz z linkami kierującymi do nich.
+</p>
+<div class="shopcreator-labels d-flex flex-row flex-wrap gap-3">
+        <div class="d-flex flex-column label-box">
+            <label for="shop-fb">Facebook</label>
+            <input class="form-input" type="text" name="shop-fb" id="shop-fb">
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-YouTube">YouTube</label>
+            <input class="form-input" type="text" name="shop-YouTube" id="shop-YouTube">
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-Instagram">Instagram</label>
+            <input class="form-input" type="text" name="shop-Instagram " id="shop-fb">
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-Tiktok">Tiktok</label>
+            <input class="form-input" type="text" name="shop-Tiktok" id="shop-Tiktok">
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-Pinterest">Pinterest</label>
+            <input class="form-input" type="text" name="shop-Pinterest" id="shop-Pinterest">
+        </div>
+        <div class="d-flex flex-column label-box">
+            <label for="shop-LindkedIn">LindkedIn</label>
+            <input class="form-input" type="text" name="shop-LindkedIn" id="shop-LindkedIn">
+        </div>
     </div>
     <div class="bottom-button right-btn">
-        <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
-        </a>
+        <button class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+        </button>
     </div>
     <div class="shopcreator-dots">
         <div class="shopcreator-dots__dot"></div>
@@ -16,4 +154,4 @@
         <div class="shopcreator-dots__dot"></div>
         <div class="shopcreator-dots__dot"></div>
     </div>
-</div>
+</form>
