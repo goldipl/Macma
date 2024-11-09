5a08246daeff671959a1fc1ca80d04390d03fbfe
goldipl <mgodlewski92@gmail.com>
2024-11-09 18:29:07 +0100

    fill & styling last seven shop creator step

--- components/shop_creator/shop_creator_step_seven_content.php
+++ components/shop_creator/shop_creator_step_seven_content.php
@@ -1,11 +1,20 @@
 <form class="shopcreator-content container">
-    <h3>Text</h3>
+    <h3>Zadanie dla chętnych :)</h3>
     <div class="textbox">
-        <p>Text</p>
+        <p>Poniższe pola nie są obowiązkowe, a Twoja strona będzie działać bez nich.</p>
     </div>
-    <div class="bottom-button right-btn">
-        <button class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
-        </button>
+    <h3 class="not-uppercase">Twoje własne style CSS</h3>
+    <div class="textbox">
+        <p>Arkusz stylów CSS to lista dyrektyw (tzw. reguł) ustalających w jaki sposób ma zostać wyświetlana przez przeglądarkęinternetową zawartość wybranego elementu (lub elementów) strony www. Można w ten sposób opisać wszystkie pojęciaodpowiedzialne za prezentację elementów dokumentów internetowych, takie jak czcionki, kolor tekstu, marginesy, odstępmiędzywierszowy lub pozycja danego elementu względem innych elementów bądź okna przeglądarki.</p>
+    </div>
+    <textarea class="form-textarea" name="css-textarea" id="css-textarea"></textarea>
+    <div class="textbox">
+        <p>Arkusz stylów CSS to lista dyrektyw (tzw. reguł) ustalających w jaki sposób ma zostać wyświetlana przez przeglądarkęinternetową zawartość wybranego elementu (lub elementów) strony www. Można w ten sposób opisać wszystkie pojęciaodpowiedzialne za prezentację elementów dokumentów internetowych, takie jak czcionki, kolor tekstu, marginesy, odstępmiędzywierszowy lub pozycja danego elementu względem innych elementów bądź okna przeglądarki.</p>
+    </div>
+    <textarea class="form-textarea" name="ga-textarea" id="ga-textarea"></textarea>
+    <div class="bottom-button">
+    <button class="orange-save-btn mt-0">Zapisz <img src="./assets/icons/dashboard/save-btn.svg" alt="save btn icon" width="18" height="18">
+    </button>
     </div>
     <div class="shopcreator-dots">
         <div class="shopcreator-dots__dot"></div>
