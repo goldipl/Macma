6dbfe60094f93adc0b3121cc34a424d511e1d463
goldipl <mgodlewski92@gmail.com>
2024-11-09 15:25:20 +0100

    fill step two shop creator content and styles

--- components/shop_creator/shop_creator_step_two_content.php
+++ components/shop_creator/shop_creator_step_two_content.php
@@ -1,7 +1,30 @@
 <div class="shopcreator-content container">
-    <h3>Text</h3>
-    <div class="textbox">
-        <p>Text</p>
+    <h3>Określ w jaki sposób chcesz korzystać ze swojej strony internetowej</h3>
+    <div class="shopcreator-type-use">
+        <div class="shopcreator-type-use__slot">
+            <label class="custom-checkbox-container">
+                <strong>Sklep internetowy</strong>
+                <span>Twoi klienci będą mogli składać zamówienia oraz wysyłać do Ciebie zapytania ofertowe i korzystać z kalkulatora produktów ze znakowaniem.</span>
+                <input type="checkbox" class="custom_input" checked>
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-type-use__slot">
+            <label class="custom-checkbox-container">
+                <strong>Katalog produktowy</strong>
+                <span>Twoi klienci będą mogli wysyłać do Ciebie zapytania ofertowe i korzystać z kalkulatora produktów ze znakowaniem. Nie będą mogli składać zamówień. </span>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
+        <div class="shopcreator-type-use__slot">
+            <label class="custom-checkbox-container">
+                <strong>Ukryj ceny</strong>
+                <span>Jeśli zaznaczysz tą opcję, w Twoim katalogu nie będą widoczne żadne ceny produktów.</span>
+                <input type="checkbox" class="custom_input">
+                <span class="custom-checkbox-checkmark"></span>
+            </label>
+        </div>
     </div>
     <div class="bottom-button right-btn">
         <a href="#" class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
