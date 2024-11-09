a6499d45db8def8922912d6e6535b0ad184ed312
goldipl <mgodlewski92@gmail.com>
2024-11-09 18:04:52 +0100

    fill & styling shop creater step 5

--- components/shop_creator/shop_creator_step_five_content.php
+++ components/shop_creator/shop_creator_step_five_content.php
@@ -1,8 +1,232 @@
 <form class="shopcreator-content container">
-    <h3>Text</h3>
+    <h3>Banery reklamowe</h3>
     <div class="textbox">
-        <p>Text</p>
+        <p>Dodaj kilka banerów reklamowych. Poniżej zobaczysz 4 gotowe banery reklamowe, które możesz wykorzystać (zmieniamy
+je sezonowo, o czym poinformujemy Cię mailowo.) 
+
+</p>
+<p>Wystarczy, że wkleisz do nich linki kierujące do produktów lub kategorii
+produktowych.
+</p>
+<p>Jeśli nie chcesz korzystać z gotowych banerów, to wówczas wgraj własne. Możesz dodać 4 banery o wymiarach 1920 x 500
+px.</p>
     </div>
+    <div class="shopcreator-banners">
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 1</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img">
+                <img src="./assets/img/shop_creator/banners/banner01.png" alt="baner" width="340" height="125">
+            </div>
+        </div>
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 2</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img">
+                <img src="./assets/img/shop_creator/banners/banner02.png" alt="baner" width="340" height="125">
+            </div>
+        </div>
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 3</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img">
+                <img src="./assets/img/shop_creator/banners/banner03.png" alt="baner" width="340" height="125">
+            </div>
+        </div>
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 4</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img">
+                <img src="./assets/img/shop_creator/banners/banner04.png" alt="baner" width="340" height="125">
+            </div>
+        </div>
+    </div>
+
+    <h3>Twoje banery</h3>
+
+    <div class="shopcreator-banners">
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 1</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img upload-icon">
+                <label for="files" class="upload-files-box">
+                    <img src="./assets/icons/upload-icon.svg" alt="upload icon" width="21" height="14">
+                    <p>Przeciągnij i upuść albo <br>
+                        <span>Dodaj plik</span>
+                    </p>
+                    <input class="file file-block" type="file" id="banner-file01">
+                </label>
+            </div>
+        </div>
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 2</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img upload-icon">
+                <label for="files" class="upload-files-box">
+                    <img src="./assets/icons/upload-icon.svg" alt="upload icon" width="21" height="14">
+                    <p>Przeciągnij i upuść albo <br>
+                        <span>Dodaj plik</span>
+                    </p>
+                    <input class="file file-block" type="file" id="banner-file02">
+                </label>
+            </div>
+        </div>
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 3</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img upload-icon">
+                <label for="files" class="upload-files-box">
+                    <img src="./assets/icons/upload-icon.svg" alt="upload icon" width="21" height="14">
+                    <p>Przeciągnij i upuść albo <br>
+                        <span>Dodaj plik</span>
+                    </p>
+                    <input class="file file-block" type="file" id="banner-file03">
+                </label>
+            </div>
+        </div>
+        <div class="shopcreator-banners-slot">
+            <div class="shopcreator-banners-slot__title">
+                <p>Baner 4</p>
+            </div>
+            <div class="shopcreator-banners-slot__checkboxes">
+                <label class="custom-checkbox-container">
+                    <strong>Włącz baner</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+                <label class="custom-checkbox-container">
+                    <strong>Ustaw jako główny</strong>
+                    <input type="checkbox" class="custom_input">
+                    <span class="custom-checkbox-checkmark"></span>
+                </label>
+            </div>
+            <div class="shopcreator-banners-slot__url">
+                <input class="form-input" type="text" name="shop-baner-url" id="shop-baner-url" placeholder="Adres url">
+            </div>
+            <div class="shopcreator-banners-slot__img upload-icon">
+                <label for="files" class="upload-files-box">
+                    <img src="./assets/icons/upload-icon.svg" alt="upload icon" width="21" height="14">
+                    <p>Przeciągnij i upuść albo <br>
+                        <span>Dodaj plik</span>
+                    </p>
+                    <input class="file file-block" type="file" id="banner-file04">
+                </label>
+            </div>
+        </div>
+    </div>
+
     <div class="bottom-button right-btn">
         <button class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
         </button>
