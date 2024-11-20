0dd70a797e756d5e353d5465759a07899e2b909f
goldipl <mgodlewski92@gmail.com>
2024-11-20 16:19:26 +0100

    update shop creator

--- components/shop_creator/shop_creator_step_six_content.php
+++ components/shop_creator/shop_creator_step_six_content.php
@@ -1,31 +1,40 @@
 <form class="shopcreator-content container">
     <h3>Klauzula danych osobowych</h3>
     <div class="textbox">
-        <p>Poniżej znajduje się przykładowa treść klauzuli niezbędnej do umieszczenia w formularzu kontaktowym na stronie.
-        </p>
+        <p>Poniżej znajduje się przykładowa treść klauzuli niezbędnej do umieszczenia w formularzu kontaktowym na stronie.</p>
         <p>Uzupełnij teksty oznaczone kolorem czerwonym, a następnie zmień kolor czcionki z czerwonego na czarny.</p>
     </div>
 
-    <textarea id="editor01"></textarea>
+    <div class="main-container">
+        <div class="editor-container editor-container_classic-editor" id="editor-container">
+            <div class="editor-container__editor"><div id="editor"></div></div>
+        </div>
+    </div>
 
     <h3>Polityka prywatności</h3>
     <div class="textbox">
-        <p>Poniżej znajduje się przykładowa treść Polityki prywatności. Uzupełnij teksty oznaczone kolorem czerwonym, a następnie zmień kolor czcionki z czerwonego na czarny.
-        </p>
+        <p>Poniżej znajduje się przykładowa treść Polityki prywatności. Uzupełnij teksty oznaczone kolorem czerwonym, a następnie zmień kolor czcionki z czerwonego na czarny.</p>
     </div>
 
-    <textarea id="editor02"></textarea>
+    <div class="main-container">
+        <div class="editor-container editor-container_classic-editor" id="editor-container">
+            <div class="editor-container__editor"><div id="editor02"></div></div>
+        </div>
+    </div>
 
     <h3>Regulamin strony </h3>
     <div class="textbox">
         <p>Poniżej znajduje się przykładowy regulamin strony. Możesz z niego skorzystać, tylko pamiętaj o uzupełnieniu brakujących informacji dotyczących Twojej firmy. Uzupełnij teksty oznaczone kolorem czerwonym, a następnie zmień kolor czcionki z czerwonego na czarny. Jeśli chcesz, możesz zmienić regulamin na własny.</p>
     </div>
 
-    <textarea id="editor03"></textarea>
+    <div class="main-container">
+        <div class="editor-container editor-container_classic-editor" id="editor-container">
+            <div class="editor-container__editor"><div id="editor03"></div></div>
+        </div>
+    </div>
 
     <div class="bottom-button right-btn">
-        <button class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
-        </button>
+        <button class="orange-btn next">Dalej <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></button>
     </div>
     <div class="shopcreator-dots">
         <div class="shopcreator-dots__dot"></div>
@@ -37,4 +46,3 @@
         <div class="shopcreator-dots__dot"></div>
     </div>
 </form>
-
--- shop-creator-step-six.php
+++ shop-creator-step-six.php
@@ -1,108 +1,114 @@
 <!DOCTYPE html>
 <html lang="pl">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MACMA - Kreator sklepów i katalogów online - Klauzula</title>
-        <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="./css/select2.min.css" />
-        <link rel="stylesheet" href="./scss/main.css">
-    </head>
-    <body>
-        <header> <?php include "./components/common/topbar.php"; ?> <?php include "./components/common/nav.php"; ?> </header>
-        <main>
-            <div class="breadcrumbs container">
-                <ul>
-                    <li class="breadcrumb-item">
-                        <a href="./index.php">Home</a>
-                    </li>
-                    <li class="breadcrumb-item">
-                        <a href="#">Kreator sklepów i katalogów online</a>
-                    </li>
-                </ul>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MACMA - Kreator sklepów i katalogów online - Klauzula</title>
+    <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/select2.min.css" />
+    <link rel="stylesheet" href="./scss/main.css">
+    <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.css">
+</head>
+<body>
+    <header> <?php include "./components/common/topbar.php"; ?> <?php include "./components/common/nav.php"; ?> </header>
+    <main>
+        <div class="breadcrumbs container">
+            <ul>
+                <li class="breadcrumb-item">
+                    <a href="./index.php">Home</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="#">Kreator sklepów i katalogów online</a>
+                </li>
+            </ul>
+        </div>
+        <div class="shop-creator-wrapper container">
+            <div class="shop-creator-title">
+                <h1>Kreator sklepów i katalogów online</h1>
             </div>
-            <div class="shop-creator-wrapper container">
-                <div class="shop-creator-title">
-                    <h1>Kreator sklepów i katalogów online</h1>
-                </div>
-                <div class="progress common-progress-bar">
-                    <div class="progress-bar" role="progressbar" aria-label="Basic example" style="width: 85.2%" aria-valuenow="85.2" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <div class="common-steps-container">
-                    <div class="container">
-                        <div class="common-steps-container-slot">
-                            <p>1. Adres</p>
-                        </div>
-                        <div class="common-steps-container-slot">
-                            <p>2. Sklep</p>
-                        </div>
-                        <div class="common-steps-container-slot">
-                            <p>3. Dane</p>
-                        </div>
-                        <div class="common-steps-container-slot">
-                            <p>4. Ceny</p>
-                        </div>
-                        <div class="common-steps-container-slot">
-                            <p>5. Banery</p>
-                        </div>
-                        <div class="common-steps-container-slot">
-                            <p class="active">6. Klauzula</p>
-                        </div>
-                        <div class="common-steps-container-slot">
-                            <p>7. Finalizacja</p>
-                        </div>
+            <div class="progress common-progress-bar">
+                <div class="progress-bar" role="progressbar" aria-label="Basic example" style="width: 85.2%" aria-valuenow="85.2" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="common-steps-container">
+                <div class="container">
+                    <div class="common-steps-container-slot">
+                        <p>1. Adres</p>
                     </div>
-                </div> <?php include "./components/shop_creator/shop_creator_step_six_content.php"; ?>
+                    <div class="common-steps-container-slot">
+                        <p>2. Sklep</p>
+                    </div>
+                    <div class="common-steps-container-slot">
+                        <p>3. Dane</p>
+                    </div>
+                    <div class="common-steps-container-slot">
+                        <p>4. Ceny</p>
+                    </div>
+                    <div class="common-steps-container-slot">
+                        <p>5. Banery</p>
+                    </div>
+                    <div class="common-steps-container-slot">
+                        <p class="active">6. Klauzula</p>
+                    </div>
+                    <div class="common-steps-container-slot">
+                        <p>7. Finalizacja</p>
+                    </div>
+                </div>
             </div>
-        </main>
-        <footer> <?php include "./components/common/footer.php"; ?> </footer>
-        <script src="./js/jquery.min.js"></script>
-        <script src="./js/popper.min.js"></script>
-        <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="./js/bootbox.min.js"></script>
-        <script src="./js/select2.min.js"></script>
-        <script src="./js/script.js"></script>
-        <!-- Text Editor JS -->
-        <script src="./js/text_editor/ckeditor.js"></script>
-        <script>
-            ClassicEditor.create(document.querySelector('#editor01'), {
-                toolbar: ['undo', 'redo', '|', 'heading', '|', 'bold', 'italic', 'link', '|', 'blockQuote', 'bulletedList', 'numberedList', '|', 'alignment'],
-                removePlugins: ['Image', 'MediaEmbed', 'CKFinder'],
-                extraPlugins: ['Image'],
-            }).catch(error => {
-                console.error(error);
-            });
-            ClassicEditor.create(document.querySelector('#editor02'), {
-                toolbar: ['undo', 'redo', '|', 'heading', '|', 'bold', 'italic', 'link', '|', 'blockQuote', 'bulletedList', 'numberedList', '|', 'alignment'],
-                removePlugins: ['Image', 'MediaEmbed', 'CKFinder'],
-                extraPlugins: ['Image'],
-            }).catch(error => {
-                console.error(error);
-            });
-            ClassicEditor.create(document.querySelector('#editor03'), {
-                toolbar: ['undo', 'redo', '|', 'heading', '|', 'bold', 'italic', 'link', '|', 'blockQuote', 'bulletedList', 'numberedList', '|', 'alignment'],
-                removePlugins: ['Image', 'MediaEmbed', 'CKFinder'],
-                extraPlugins: ['Image'],
-            }).catch(error => {
-                console.error(error);
+            <?php include "./components/shop_creator/shop_creator_step_six_content.php"; ?>
+        </div>
+    </main>
+    <footer> <?php include "./components/common/footer.php"; ?> </footer>
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="./js/bootbox.min.js"></script>
+    <script src="./js/select2.min.js"></script>
+    <script src="./js/script.js"></script>
+    <!-- Text Editor JS -->
+    <script type="importmap">
+    {
+        "imports": {
+            "ckeditor5": "https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.js",
+            "ckeditor5/": "https://cdn.ckeditor.com/ckeditor5/43.3.1/"
+        }
+    }
+    </script>
+    <script type="module" src="./js/text_editor/ckeditor.js">
+
+        // Initialize the first editor
+        ClassicEditor.create(document.querySelector('#editor'))
+            .then(editor => {
+                console.log('Editor 1 is ready', editor);
+            })
+            .catch(error => {
+                console.error('Error initializing editor 1:', error);
             });
 
-            document.addEventListener('keydown', function(event) {
-                const activeElement = document.activeElement;
-
-                if (event.key === ' ' && activeElement.tagName === 'DIV' && activeElement.classList.contains('ck-editor__editable')) {
-                    event.stopPropagation(); 
-                }
+        // Initialize the second editor
+        ClassicEditor.create(document.querySelector('#editor02'))
+            .then(editor => {
+                console.log('Editor 2 is ready', editor);
+            })
+            .catch(error => {
+                console.error('Error initializing editor 2:', error);
             });
-        </script>
-        <style>
-            .ck-editor__editable {
-                outline: none;
-                overflow-wrap: break-word;
-                white-space: pre-wrap;
-            }
-        </style>
-    </body>
+
+        // Initialize the third editor
+        ClassicEditor.create(document.querySelector('#editor03'))
+            .then(editor => {
+                console.log('Editor 3 is ready', editor);
+            })
+            .catch(error => {
+                console.error('Error initializing editor 3:', error);
+            });
+    </script>
+    <style>
+        .ck-content {
+            line-height: 1.6;
+            word-break: break-word;
+        }
+    </style>
+</body>
 </html>
